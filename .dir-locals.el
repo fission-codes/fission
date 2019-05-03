4d66f3af12f45a9127dcf4eb6ba2065767d234eb
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-05-02 19:53:37 -0700

    Remove local file

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((haskell-mode
-  (intero-targets "ipfs-api:lib" "ipfs-api:exe:server" "ipfs-api:test:tracks-doctest")))
