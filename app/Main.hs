bd83f3414f392cac0e20cb9be37451e403ec3fcf
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-05-08 14:27:41 -0700

    Setup EKG Monitoring (#10)

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import RIO
 
 import Network.Wai.Handler.Warp
 import Network.Wai.Logger
+import System.Remote.Monitoring.Wai
 
 import qualified Fission.Ambient as Ambient
 import qualified Fission.Env     as Env
@@ -14,6 +15,12 @@ import qualified Fission.Web     as Web
 
 main :: IO ()
 main = do
+  -- TODO only run locally in dev
+  ekgHost <- Ambient.lookup "EKG_HOST" "localhost"
+  ekgPort <- Ambient.lookup "EKG_PORT" 8888
+
+  void $ forkServer ekgHost ekgPort
+
   port <- Ambient.lookup "PORT" 8000
 
   withStdoutLogger $ \logger -> do
@@ -24,4 +31,5 @@ main = do
 
     runRIO env $ do
       logInfo $ "Servant running at port " <> display port <> "\n"
+      logInfo $ "EKG available at " <> displayBytesUtf8 ekgHost <> ":" <> display ekgPort <> "\n"
       liftIO $ runSettings settings $ Web.app env
