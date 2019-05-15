7d275aa52ef03c89203a30b5fbc346f2a382c743
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-05-15 11:42:47 -0700

    Save

--- src/Fission/User.hs
+++ src/Fission/User.hs
@@ -1,47 +1,21 @@
-{-# LANGUAGE DeriveAnyClass        #-}
-{-# LANGUAGE DeriveGeneric         #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE GADTs                 #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NoImplicitPrelude     #-}
-{-# LANGUAGE StandaloneDeriving    #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeApplications      #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE TypeSynonymInstances  #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 
-module Fission.User
-  ( UserT (..)
-  , User
-  , UserId
-  , userEmail
-  , userPassword
-  ) where
+module Fission.User where
 
 import RIO
 
-import Control.Lens (makeLenses)
+import Database.Persist.Sqlite
+import Database.Persist.TH
 
-import Database.Beam
-import Database.Beam.Sqlite ()
-
-data UserT f = User
-  { _userEmail    :: Columnar f Text
-  , _userPassword :: Columnar f Text
-  }
-  deriving (Generic, Beamable)
-
-makeLenses ''UserT
-
-type User   =            UserT Identity
-type UserId = PrimaryKey UserT Identity
-
-deriving instance Show User
-deriving instance Eq User
-
-instance Table UserT where
-  data PrimaryKey UserT f = UserId (C f Text)
-    deriving (Generic, Beamable)
-
-  primaryKey = UserId . _userEmail
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+  User
+      email    Text
+      password Text
+      deriving Show
+|]
