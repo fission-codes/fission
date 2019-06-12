3759354764d067d1e1b5336b1458e87e172d7fec
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-06-12 10:34:10 -0700

    Remove unused imports

--- src/Fission/Storage/SQLite/Internal.hs
+++ src/Fission/Storage/SQLite/Internal.hs
@@ -4,12 +4,8 @@ module Fission.Storage.SQLite.Internal (traceAll) where
 
 import RIO
 
-import Control.Lens (makeLenses)
-import Data.Has
 import Database.Selda
 import Database.Selda.SQLite
-
-import Fission.Config (DBPool)
 
 traceAll :: (Show a, Relational a) => Table a -> IO ()
 traceAll tbl = withSQLite "ipfs-api.sqlite" do
