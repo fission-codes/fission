a97a388ae0ff57418f288da3d14854c48bab8e80
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-06-14 12:59:15 -0700

    Configurable hostname for Heroku response (#34)

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,11 @@
 module Main (main) where
 
 import RIO
-import RIO.Char (toLower)
 
+import Control.Lens ((.~))
 import Data.Aeson (decodeFileStrict)
 import System.Environment
 
-import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Logger
 import Network.Wai.Middleware.RequestLogger
@@ -14,8 +13,10 @@ import Network.Wai.Middleware.RequestLogger
 import Fission.Config         as Config
 import Fission.Storage.SQLite as SQLite
 
-import Fission.Internal.Orphanage ()
+import           Fission.Internal.Orphanage ()
+import qualified Fission.Internal.UTF8 as UTF8
 
+import           Fission.Environment
 import qualified Fission.Log                            as Log
 import qualified Fission.Monitor                        as Monitor
 import qualified Fission.Web                            as Web
@@ -27,35 +28,25 @@ import           Fission.Platform.Heroku.AddOn.Manifest hiding (id)
 main :: IO ()
 main = withStdoutLogger $ \stdOut -> do
   Just (manifest :: Manifest) <- decodeFileStrict "./addon-manifest.json"
-  pool <- simply setupPool
-  runRIO (mkConfig manifest pool) do
+  pool      <- simply setupPool
+  hostURL   <- withEnv "HOST" "localhost:3000" UTF8.textShow
+  condDebug <- withFlag "DEBUG_REQS" id logStdoutDev
+
+  runRIO (mkConfig manifest pool hostURL) do
     condMonitor
     Web.Config.Config port <- Web.Config.get
     logInfo $ "Servant running at port " <> display port
-
-    debuggable <- liftIO condDebugReqs
-    app        <- Web.app =<< ask
-    liftIO . runSettings (mkSettings stdOut port) $ debuggable app
+    liftIO . runSettings (mkSettings stdOut port) . condDebug =<< Web.app =<< ask
 
 simply :: RIO LogFunc a -> IO a
 simply = runRIO (mkLogFunc Log.simple)
 
-condDebugReqs :: IO (Application -> Application)
-condDebugReqs = do
-  debugReqsEnv <- liftIO $ lookupEnv "DEBUG_REQS"
-  return $ maybe id logOrNot debugReqsEnv
+mkConfig :: Manifest -> SeldaPool -> Text -> Config
+mkConfig manifest pool url = cfg & host .~ Host url
   where
-    logOrNot :: String -> (Application -> Application)
-    logOrNot str =
-      if fmap toLower str == "true"
-         then logStdoutDev
-         else id
-
-condMonitor :: HasLogFunc cfg => RIO cfg ()
-condMonitor = do
-  monitorEnv <- liftIO $ lookupEnv "MONITOR"
-  monitored  <- return $ maybe False ((== "true") . fmap toLower) monitorEnv
-  when monitored Monitor.wai
+    cfg   = Config.base hID hPass (DBPool pool)
+    hID   = HerokuID       . encodeUtf8 $ manifest ^. Manifest.id
+    hPass = HerokuPassword . encodeUtf8 $ manifest ^. api ^. password
 
 mkSettings :: ApacheLogger -> Port -> Settings
 mkSettings stdOut port = portSettings $ logSettings defaultSettings
@@ -63,11 +54,10 @@ mkSettings stdOut port = portSettings $ logSettings defaultSettings
     portSettings = setPort port
     logSettings  = setLogger stdOut
 
-mkConfig :: Manifest -> SeldaPool -> Config
-mkConfig manifest pool = Config.base hID hPass (DBPool pool)
-  where
-    hID   = HerokuID       . encodeUtf8 $ manifest ^. Manifest.id
-    hPass = HerokuPassword . encodeUtf8 $ manifest ^. api ^. password
+condMonitor :: HasLogFunc cfg => RIO cfg ()
+condMonitor = do
+  monitorFlag <- liftIO $ getFlag "MONITOR"
+  when monitorFlag Monitor.wai
 
 setupPool :: HasLogFunc cfg => RIO cfg SeldaPool
 setupPool = do
