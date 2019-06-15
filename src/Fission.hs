ed2448d5880f938e95c2406f7e67351e34898258
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-06-15 00:45:36 -0700

    Remove dead code

--- src/Fission.hs
+++ src/Fission.hs
@@ -1,6 +1,5 @@
 module Fission
   ( Fission
-  -- , mkConfig
   , fromConfig
   , simply
   ) where
@@ -15,19 +14,6 @@ import qualified Fission.Log                   as Log
 import qualified Fission.Platform.Heroku.Types as Heroku
 import qualified Fission.Storage.Types         as DB
 import qualified Fission.Web.Types             as Web
-
--- -- FIXME with Envy
--- mkConfig :: Heroku.ID -> Heroku.Password -> DB.Pool -> Config
--- mkConfig hkuID hkuPass pool = Config
---     { _logFunc        = mkLogFunc Log.simple
---     , _minLogLevel    = Log.MinLogLevel LevelDebug
---     , _ipfsPath       = IPFS.Path "/usr/local/bin/ipfs"
---     , _host           = Web.Host "localhost:3000"
---     , _dbPath         = DB.Path "ipfs-api.sqlite"
---     , _dbPool         = pool
---     , _herokuID       = hkuID
---     , _herokuPassword = hkuPass
---     }
 
 fromConfig :: (MonadReader cfg m, Has a cfg) => m a
 fromConfig = view hasLens
