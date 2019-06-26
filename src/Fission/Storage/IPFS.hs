30963815556249740c958a8e20c411f3e2033b8f
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-06-26 13:56:13 -0700

    Working on returning JSON structure from Multipart

--- src/Fission/Storage/IPFS.hs
+++ src/Fission/Storage/IPFS.hs
@@ -1,10 +1,14 @@
-module Fission.Storage.IPFS (add) where
+module Fission.Storage.IPFS
+  ( add
+  , addFile
+  ) where
 
 import           RIO
 import qualified RIO.ByteString.Lazy as Lazy
 import           RIO.Process (HasProcessContext)
 
 import Data.Has
+import Data.ByteString.Lazy.Char8 as BS
 
 import           Fission.Internal.Constraint
 import           Fission.IPFS.Types          as IPFS
@@ -16,4 +20,42 @@ add :: MonadRIO          cfg m
     => Has IPFS.Path     cfg
     => Lazy.ByteString
     -> m IPFS.Address
-add input = mkAddress <$> IPFS.Proc.run ["add", "-q"] input
+add raw = mkAddress <$> IPFS.Proc.run ["add", "-q"] raw
+
+addFile :: MonadRIO cfg m
+        => HasProcessContext cfg
+        => HasLogFunc        cfg
+        => Has IPFS.Path     cfg
+        => Lazy.ByteString
+        -> String
+        -> m Dir
+addFile raw name = do
+  result <- fmap mkAddress . BS.lines <$> IPFS.Proc.run opts raw
+  case result of
+    [inner, outer] -> return $ Root outer (File inner)
+
+  where
+    opts = [ "add"
+           , "-wq"
+           , "--stdin-name"
+           , name
+           ]
+
+data Dir
+  = File IPFS.Address
+  | Dir (Map FileName Dir)
+  | Root IPFS.Address Dir
+
+type FileName = String
+
+
+-- addFile :: MonadRIO cfg m
+--     => HasProcessContext cfg
+--     => HasLogFunc        cfg
+--     => Has IPFS.Path     cfg
+--     => Lazy.ByteString
+--     -> Text
+--     -> m IPFS.Address
+-- addFile raw filename = do
+--   innerHash <- addRaw raw
+--   return [(filename, innerHash)]
