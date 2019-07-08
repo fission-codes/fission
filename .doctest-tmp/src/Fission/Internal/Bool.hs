ec704d6b8bcd37957f6c4e475a28646b7861991d
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-07-07 17:35:28 -0700

    Remove doctest tmp

--- .doctest-tmp/src/Fission/Internal/Bool.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE ApplicativeDo, BangPatterns, BlockArguments, ConstraintKinds, DataKinds, DeriveAnyClass, DeriveFoldable, DeriveFunctor, DeriveGeneric, DeriveLift, DeriveTraversable, DerivingStrategies, FlexibleContexts, FlexibleInstances, FunctionalDependencies, GADTs, GeneralizedNewtypeDeriving, KindSignatures, LambdaCase, LiberalTypeSynonyms, MultiParamTypeClasses, MultiWayIf, NamedFieldPuns, NoImplicitPrelude, NoMonomorphismRestriction, OverloadedStrings, OverloadedLabels, OverloadedLists, RankNTypes, RecordWildCards, ScopedTypeVariables, StandaloneDeriving, TupleSections, TypeSynonymInstances, TemplateHaskell, TypeOperators, ViewPatterns #-}
-module Fission.Internal.Bool (anyX) where
-
-import RIO
-
-anyX :: [a -> Bool] -> a -> Bool
-anyX preds value = True `elem` (preds <*> [value])
--- src/Fission/Environment.hs
+++ src/Fission/Environment.hs
@@ -12,10 +12,11 @@ import RIO.Char (toLower)
 import System.Environment (lookupEnv)
 import System.Envy
 
+-- | Get an environment variable. @error@s if not found.
 getEnv :: FromEnv a => IO a
 getEnv = decodeEnv >>= \case
-  Left msg  -> error msg
-  Right val -> return val
+  Left  msg -> error msg
+  Right val -> pure val
 
 -- | Fallback value for a monadic lookup
 --
@@ -30,9 +31,17 @@ getEnv = decodeEnv >>= \case
 (.!~) :: Monad m => m (Maybe a) -> a -> m a
 mVal .!~ fallback = pure (fromMaybe fallback) <*> mVal
 
+-- | Switch on an environment flag
+--
+-- > withFlag "DEBUG" "nope" "yep"
+-- "nope"
 withFlag :: String -> a -> a -> IO a
 withFlag key whenFalse whenTrue = withEnv key whenFalse (const whenTrue)
 
+-- | Perform actions on an environment variable, with fallback if not available
+--
+-- > withEnv "PORT" 80 (* 2)
+-- 80
 withEnv :: String -> a -> (String -> a) -> IO a
 withEnv key fallback transform = pure (maybe fallback transform) <*> lookupEnv key
 
