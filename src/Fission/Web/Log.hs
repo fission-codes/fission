402cceaf78b640c023dfdd6060e466177957ef69
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-07-09 23:21:55 -0700

    Style

--- src/Fission/Web/Log.hs
+++ src/Fission/Web/Log.hs
@@ -20,29 +20,29 @@ rioApacheLogger :: MonadRIO   cfg m
                 -> Maybe Integer
                 -> m ()
 rioApacheLogger Request {..} Status {statusCode} _mayInt =
-    if | statusCode >= 500 -> logError formatted
-       | statusCode >= 400 -> logInfo  formatted
-       | otherwise        -> logDebug formatted
-      where
-        formatted :: Utf8Builder
-        formatted = mconcat
-          [ displayShow remoteHost
-          , " - - "
-          , displayShow httpVersion
-          , " "
-          , displayShow requestMethod
-          , " "
-          , if rawPathInfo    == "" then "" else displayShow rawPathInfo
-          , if rawQueryString == "" then "" else displayShow rawQueryString
-          , " "
-          , displayShow statusCode
-          , displayShow $ maybe "" (" - " <>) requestHeaderUserAgent
-          ]
+  if | statusCode >= 500 -> logError formatted
+     | statusCode >= 400 -> logInfo  formatted
+     | otherwise        -> logDebug formatted
+  where
+    formatted :: Utf8Builder
+    formatted = mconcat
+      [ displayShow remoteHost
+      , " - - "
+      , displayShow httpVersion
+      , " "
+      , displayShow requestMethod
+      , " "
+      , if rawPathInfo    == "" then "" else displayShow rawPathInfo
+      , if rawQueryString == "" then "" else displayShow rawQueryString
+      , " "
+      , displayShow statusCode
+      , displayShow $ maybe "" (" - " <>) requestHeaderUserAgent
+      ]
 
 mkSettings :: LogFunc -> Port -> Settings
 mkSettings logger port = defaultSettings
-                        & setPort port
-                        & setLogger (fromLogFunc logger)
+                       & setPort port
+                       & setLogger (fromLogFunc logger)
 
 fromLogFunc :: LogFunc -> ApacheLogger
 fromLogFunc logger = \r s mi -> runRIO logger (rioApacheLogger r s mi)
