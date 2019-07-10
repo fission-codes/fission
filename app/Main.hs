ec685c74e5781d0f8d5b5eeff34962586068a240
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-07-09 21:57:14 -0700

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -26,6 +26,10 @@ import qualified Fission.Platform.Heroku.AddOn.Manifest as Manifest
 import           Fission.Platform.Heroku.AddOn.Manifest hiding (id)
 import qualified Fission.Platform.Heroku.Types          as Heroku
 
+import Network.HTTP.Types.Status
+import           Fission.Internal.Constraint
+import Network.Wai
+
 main :: IO ()
 main = withStdoutLogger $ \stdOut -> do
   Web.Port port <- getEnv
@@ -38,17 +42,21 @@ main = withStdoutLogger $ \stdOut -> do
   _dbPath       <- decode .!~ DB.Path "ipfs-api.sqlite"
   _dbPool       <- RIO.runSimpleApp $ SQLite.connPool _dbPath
 
-  let
-    _herokuID       = Heroku.ID       . encodeUtf8 $ manifest ^. Manifest.id
-    _herokuPassword = Heroku.Password . encodeUtf8 $ manifest ^. api ^. password
-    _logFunc        = mkLogFunc Log.simple
-    config          = Config { .. }
+  let isVerbose = True -- get from the command line instead
+  logOptions' <- logOptionsHandle stdout isVerbose
+  let logOptions = setLogUseTime True logOptions'
 
-  runRIO config do
-    condMonitor
-    logDebug $ "Configured with: " <> displayShow config
-    logInfo  $ "Servant running at: " <> display port
-    liftIO . runSettings (mkSettings stdOut port) . condDebug =<< Web.app config
+  withLogFunc logOptions $ \_logFunc -> do
+    let
+      _herokuID       = Heroku.ID       . encodeUtf8 $ manifest ^. Manifest.id
+      _herokuPassword = Heroku.Password . encodeUtf8 $ manifest ^. api ^. password
+      config          = Config {..}
+
+    runRIO config do
+      condMonitor
+      logDebug $ "Configured with: " <> displayShow config
+      logInfo  $ "Servant running at: " <> display port
+      liftIO . runSettings (mkSettings' _logFunc port) . condDebug =<< Web.app config
 
 mkSettings :: ApacheLogger -> Port -> Settings
 mkSettings stdOut port = portSettings $ logSettings defaultSettings
@@ -56,7 +64,33 @@ mkSettings stdOut port = portSettings $ logSettings defaultSettings
     portSettings = setPort port
     logSettings  = setLogger stdOut
 
+mkSettings' :: LogFunc -> Port -> Settings
+mkSettings' logger port = defaultSettings
+                        & setPort port
+                        & setLogger (rioApacheLogger logger)
+
 condMonitor :: HasLogFunc cfg => RIO cfg ()
 condMonitor = do
   monitorFlag <- liftIO $ getFlag "MONITOR"
   when monitorFlag Monitor.wai
+
+rioApacheLogger :: LogFunc -> ApacheLogger
+rioApacheLogger logger = \req status@(Status {..}) _mayInt ->
+  runRIO logger $ output req statusCode -- (Request -> Status -> Maybe Integer -> IO ())
+
+output :: (MonadRIO cfg m, HasLogFunc cfg) => Request -> Int -> m ()
+output req statusCode | statusCode >= 500 = logError $ displayShow req
+                      | statusCode >= 400 = logInfo $ displayShow req
+                      | otherwise        = logDebug $ displayShow req
+
+withStdoutLogger :: (ApacheLogger -> IO a) -> IO a
+withStdoutLogger app = bracket setup teardown $ \(aplogger, _) ->
+    app aplogger
+  where
+    setup = do
+      tgetter <- newTimeCache simpleTimeFormat
+      apf     <- initLogger FromFallback (LogStdout 4096) tgetter
+      let aplogger = apacheLogger apf
+          remover  = logRemover apf
+      return (aplogger, remover)
+    teardown (_, remover) = void remover
