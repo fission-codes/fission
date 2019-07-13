0f4103931fafb45673b7138ea1821ab03a9a2c99
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-07-13 12:20:12 -0700

    Explicit exports

--- src/Fission/Platform/Heroku/AddOn/Lens.hs
+++ src/Fission/Platform/Heroku/AddOn/Lens.hs
@@ -1,4 +1,10 @@
-module Fission.Platform.Heroku.AddOn.Lens where
+module Fission.Platform.Heroku.AddOn.Lens
+  ( addOnID
+  , uuid
+  , region
+  , insertedAt
+  , modifiedAt
+  ) where
 
 import Control.Lens (makeLenses)
 import Fission.Platform.Heroku.AddOn.Types
