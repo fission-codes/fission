0d615c60ef900169d44164d1e9155578fe482c02
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-07-14 22:30:33 -0700

    Remove dead code

--- src/Fission/Web/Error.hs
+++ src/Fission/Web/Error.hs
@@ -1,14 +1,10 @@
+-- | Web error handling, common patterns, and other helpers
 module Fission.Web.Error
-  ( checkUnicode
-  , ensure
+  ( ensure
   , throw
   ) where
 
 import RIO
-
-import Servant
-
-import qualified Fission.Internal.UTF8 as UTF8
 
 import Data.Aeson
 import Network.HTTP.Types.Status
@@ -16,9 +12,16 @@ import Servant.Exception
 
 import Fission.Internal.Constraint
 
-checkUnicode :: (MonadThrow m, Show a) => Either a b -> m b
-checkUnicode (Right ok) = pure ok
-checkUnicode (Left err) = throwM $ err500 { errBody = UTF8.showLazyBS err }
+ensure :: MonadRIO   cfg m
+       => HasLogFunc cfg
+       => MonadThrow     m
+       => Display      err
+       => Exception    err
+       => ToJSON       err
+       => ToServantErr err
+       => Either err b
+       -> m ()
+ensure = either throw (pure . const ())
 
 throw :: MonadRIO   cfg m
       => HasLogFunc cfg
@@ -32,14 +35,3 @@ throw :: MonadRIO   cfg m
 throw err = do
   when (statusIsServerError $ status err) (logError $ display err)
   throwM $ toServantException err
-
-ensure :: MonadRIO   cfg m
-       => HasLogFunc cfg
-       => MonadThrow     m
-       => Display      err
-       => Exception    err
-       => ToJSON       err
-       => ToServantErr err
-       => Either err b
-       -> m ()
-ensure = either throw (pure . const ())
