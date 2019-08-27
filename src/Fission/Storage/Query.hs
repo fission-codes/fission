68207e63ade9ac54352d8438e893ba22a5516da2
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-08-26 18:37:49 -0700

    Reword

--- src/Fission/Storage/Query.hs
+++ src/Fission/Storage/Query.hs
@@ -16,7 +16,18 @@ is' row prop = row ! prop .== true
 all' :: Row s t -> [Row s t -> Col s Bool] -> Col s Bool
 all' row = foldr (\prop acc -> acc .&& prop row) true
 
-findOne :: (MonadSelda f, Result (OuterCols a)) => Query (Inner s) a -> f (Maybe (Res (OuterCols a)))
+findOne :: MonadSelda m
+        => Result (OuterCols a)
+        => Query (Inner s) a
+        -> m (Maybe (Res (OuterCols a)))
 findOne = fmap headMaybe . query . limit 0 1
 
+
+oneEq :: MonadSelda m
+      => Relational t
+      => SqlType r
+      => Table t
+      -> Selector t r
+      -> r
+      -> m (Maybe t)
 oneEq tbl selector target = findOne $ select tbl `suchThat` (selector `is` target)
