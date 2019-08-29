6cfb0e6315711cc6dda6e4a45c419ac22be83065
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-08-29 14:38:18 -0700

    allwo headers

--- src/Fission/Web/CORS.hs
+++ src/Fission/Web/CORS.hs
@@ -1,5 +1,6 @@
 module Fission.Web.CORS
   ( allowedMethods
+  , allowedRequestHeaders
   , middleware
   , policy
   ) where
@@ -11,10 +12,13 @@ import qualified Network.Wai                 as WAI
 import           Network.Wai.Middleware.Cors
 
 middleware :: WAI.Middleware
-middleware = cors (const $ Just simpleCorsResourcePolicy)
+middleware = cors (const $ Just policy)
 
 policy :: CorsResourcePolicy
-policy = simpleCorsResourcePolicy { corsMethods = allowedMethods }
+policy = simpleCorsResourcePolicy
+  { corsMethods        = allowedMethods
+  , corsRequestHeaders = allowedRequestHeaders
+  }
 
 allowedMethods :: [Method]
 allowedMethods = [ methodGet
@@ -25,3 +29,9 @@ allowedMethods = [ methodGet
                  , methodPost
                  , methodPut
                  ]
+
+allowedRequestHeaders :: [HeaderName]
+allowedRequestHeaders = [ hAuthorization
+                        , hContentEncoding
+                        , hContentType
+                        ]
