e0187e1af4d4fcbf7df6369f8d67c8bd85ffd182
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-09-05 01:15:00 -0700

    Clean

--- src/Fission/IPFS/Client/Add.hs
+++ src/Fission/IPFS/Client/Add.hs
@@ -1,12 +1,10 @@
 module Fission.IPFS.Client.Add (API) where
 
-import           RIO
 import qualified RIO.ByteString.Lazy as Lazy
 
 import Servant
 
-import           Fission.IPFS.CID.Types (CID)
--- import qualified Fission.File.Types        as File
+import Fission.IPFS.CID.Types (CID)
 
 type API = ReqBody '[PlainText] Lazy.ByteString -- Text
         :> Post    '[PlainText] CID
