6f7428126c6a1b2477a483bea2a3bb816de9b540
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-09-20 16:51:22 -0700

    Mostly working, but pins now enforce conatraints by throwing. Need to fix / make idempotent

--- library/Fission/Storage/PostgreSQL/Migrate.hs
+++ library/Fission/Storage/PostgreSQL/Migrate.hs
@@ -6,12 +6,12 @@ module Fission.Storage.PostgreSQL.Migrate
 
 import RIO
 
+import           Database.Selda.PostgreSQL
 import qualified Fission.Platform.Heroku.AddOn as Heroku.AddOn.Table
 import           Fission.Storage.PostgreSQL    (makeTable)
 import           Fission.Storage.Types         as DB
 import qualified Fission.User.CID.Table        as UserCID.Table
 import qualified Fission.User.Table            as User.Table
-import           Database.Selda.PostgreSQL
 
 -- | Table creation or migration
 type Mutation = IO ()
@@ -33,7 +33,7 @@ mutations :: Text
   -> Maybe Text
   -> Maybe Text
   -> [Mutation]
-mutations h p d s u pass =  mutations' $ DB.PGInfo $ (PGConnectInfo h p d s u pass)
+mutations h p d s u pass =  mutations' $ DB.PGInfo $ PGConnectInfo h p d s u pass
 
 mutations' :: DB.PGInfo -> [Mutation]
 mutations' db =
--- library/Fission/Storage/Types.hs
+++ library/Fission/Storage/Types.hs
@@ -6,15 +6,16 @@ module Fission.Storage.Types
 
 import RIO
 
-import qualified Data.Pool              as Database
-import           Database.Selda.Backend (SeldaConnection)
-import           Database.Selda.PostgreSQL
 import qualified Data.ByteString.Char8     as BS
+import qualified Data.Pool                 as Database
+import           Database.Selda.Backend    (SeldaConnection)
+import           Database.Selda.PostgreSQL
 
 type SeldaPool = Database.Pool (SeldaConnection PG)
 
 newtype Pool = Pool { getPool :: SeldaPool }
   deriving Show
+
 newtype PGInfo = PGInfo { getPGInfo :: PGConnectInfo }
 
 instance Show PGInfo where
