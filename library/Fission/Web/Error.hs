2b91c41c07f1db42ef1b3dd981880780f59be65b
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-10-11 21:46:33 +0900

    Int -> ServerError

--- library/Fission/Web/Error.hs
+++ library/Fission/Web/Error.hs
@@ -21,6 +21,45 @@ class ToServerError err where
 instance ToServerError ServerError where
   toServerError = id
 
+instance ToServerError Int where
+  toServerError = \case
+    300 -> err300
+    301 -> err301
+    302 -> err302
+    303 -> err303
+    304 -> err304
+    305 -> err305
+    307 -> err307
+
+    400 -> err400
+    401 -> err401
+    402 -> err402
+    403 -> err403
+    404 -> err404
+    405 -> err405
+    406 -> err406
+    407 -> err407
+    409 -> err409
+    410 -> err410
+    411 -> err411
+    412 -> err412
+    413 -> err413
+    414 -> err414
+    415 -> err415
+    416 -> err416
+    417 -> err417
+    418 -> err418
+    422 -> err422
+
+    500 -> err500
+    501 -> err501
+    502 -> err502
+    503 -> err503
+    504 -> err504
+    505 -> err505
+
+    n -> error $ show n <> " is not an error status code"
+
 ensure :: MonadRIO   cfg m
        => HasLogFunc cfg
        => MonadThrow     m
