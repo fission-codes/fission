9fb62f942ff9ddca9a3a9d11ac67c2239e0ff8fc
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-10-11 20:02:16 +0900

    Fix rebase typos

--- library/Fission/Web/Routes.hs
+++ library/Fission/Web/Routes.hs
@@ -9,15 +9,12 @@ module Fission.Web.Routes
 
 import Servant
 
-import qualified Fission.Web.IPFS        as IPFS
-import qualified Fission.Web.Auth.Verify as Auth.Verify
-import qualified Fission.Web.Ping        as Ping
-import qualified Fission.Web.Heroku      as Heroku
-import qualified Fission.Web.DNS         as DNS
-import qualified Fission.Web.Auth        as Auth
-
-import           Fission.User.Types
-import qualified Fission.Web.User        as User
+import qualified Fission.Web.IPFS   as IPFS
+import qualified Fission.Web.Ping   as Ping
+import qualified Fission.Web.Heroku as Heroku
+import qualified Fission.Web.DNS    as DNS
+import qualified Fission.Web.Auth   as Auth
+import qualified Fission.Web.User   as User
 
 type API = IPFSRoute
       :<|> HerokuRoute
@@ -36,11 +33,6 @@ type HerokuRoute = "heroku"
                    :> Auth.HerokuAddOnAPI
                    :> Heroku.API
 
-type IPFSRoute  = IPFSPrefix :> IPFS.API
-type IPFSPrefix = "ipfs"
-
-type PingRoute = "ping" :> Ping.API
-
 type DNSRoute = "dns"
-                  :> Auth.ExistingUser
-                  :> DNS.API
+                :> Auth.ExistingUser
+                :> DNS.API
