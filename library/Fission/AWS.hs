197fd79bf48276a635d7227ceef88bb1e5f4d8fb
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-10-11 21:16:25 +0900

    Clean up error handling

---
+++ library/Fission/AWS.hs
@@ -0,0 +1,50 @@
+module Fission.AWS
+  ( createEnv
+  , ensureContent
+  , validate
+  , withAWS
+  ) where
+
+import RIO
+
+import Data.Has
+import Servant
+
+import Network.AWS
+import Network.AWS.Auth as AWS
+import Network.AWS.Route53
+
+import qualified Fission.Config as Config
+import           Fission.Internal.Constraint
+import           Fission.Web.Error
+
+withAWS :: (MonadUnliftIO m, HasEnv r) => r -> Region -> AWS a -> m a
+withAWS env region = runResourceT . runAWS env . within region
+
+createEnv :: MonadRIO           cfg m
+          => Has AWS.AccessKey cfg
+          => Has AWS.SecretKey cfg
+          => m Env
+createEnv = do
+  accessKey :: AccessKey <- Config.get
+  secretKey :: SecretKey <- Config.get
+  liftIO $ newEnv $ FromKeys accessKey secretKey
+
+validate :: ChangeResourceRecordSetsResponse -> Either ServerError ChangeResourceRecordSetsResponse
+validate changeSet
+  | status >= 500 = Left err500 { errBody = "Unkown AWS Error" }
+  | status >= 400 = Left err404 { errBody = "Could not locate AWS Resource" }
+  | otherwise = Right changeSet
+  where
+    status = changeSet ^. crrsrsResponseStatus
+
+ensureContent
+  :: MonadRIO   cfg m
+  => MonadThrow     m
+  => Exception err
+  => m (Either err ChangeResourceRecordSetsResponse)
+  -> m ChangeResourceRecordSetsResponse
+ensureContent runRequest = do
+  errOrResp <- runRequest
+  resp      <- ensureM errOrResp
+  ensureM $ validate resp
