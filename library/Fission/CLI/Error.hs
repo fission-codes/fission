3c1ec136c2378a833f9e443b7e0c370400de5876
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-10-14 00:23:06 +0900

    Remove dead code

--- library/Fission/CLI/Error.hs
+++ library/Fission/CLI/Error.hs
@@ -1,4 +1,4 @@
-module Fission.CLI.Error where
+module Fission.CLI.Error (cliLog) where
 
 import RIO
 
@@ -9,29 +9,3 @@ import qualified Fission.CLI.Display.Error as CLI.Error
 
 cliLog :: (MonadRIO cfg m, HasLogFunc cfg) => Error -> m ()
 cliLog = CLI.Error.put'
-
--- eitherCLI :: ToError err => Either err b -> Either Error b
--- eitherCLI = either (Left . toError) Right
-
--- import qualified Fission.CLI.Auth   as Auth
--- import qualified Fission.IPFS.Error as IPFS
-
--- data Error
---   = AuthErr Auth.CLIError
---   | IPFSErr IPFS.Error
---   deriving ( Exception
---            , Eq
---            , Show
---            )
-
--- class ToError err where
---   toError :: err -> Error
-
--- instance ToError Auth.CLIError where
---   toError authErr = AuthErr authErr
-
--- instance ToError IPFS.Error where
---   toError ipfsErr = IPFSErr ipfsErr
-
--- instance ToError IPFS.Add where
---   toError = toError . IPFS.AddErr
