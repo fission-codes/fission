b8506c3521e4146a188965030510995d01e21be7
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-10-13 23:28:12 +0900

    Unrearrange

--- library/Fission/Web/Routes.hs
+++ library/Fission/Web/Routes.hs
@@ -10,11 +10,11 @@ module Fission.Web.Routes
 
 import Servant
 
-import qualified Fission.Web.Auth   as Auth
-import qualified Fission.Web.DNS    as DNS
-import qualified Fission.Web.Heroku as Heroku
 import qualified Fission.Web.IPFS   as IPFS
 import qualified Fission.Web.Ping   as Ping
+import qualified Fission.Web.Heroku as Heroku
+import qualified Fission.Web.DNS    as DNS
+import qualified Fission.Web.Auth   as Auth
 import qualified Fission.Web.User   as User
 
 type API = IPFSRoute
