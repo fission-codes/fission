de66f3a2ae79af3a516a31617d0335887e627ccb
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-10-13 23:19:20 +0900

    Remove duplicate imports (from messy merge)

--- library/Fission/Web/Routes.hs
+++ library/Fission/Web/Routes.hs
@@ -11,23 +11,12 @@ module Fission.Web.Routes
 
 import Servant
 
-import qualified Fission.Web.IPFS        as IPFS
-import qualified Fission.Web.Ping        as Ping
-import qualified Fission.Web.Heroku      as Heroku
-import qualified Fission.Web.DNS         as DNS
-import qualified Fission.Web.Auth        as Auth
-
+import qualified Fission.Web.Auth   as Auth
+import qualified Fission.Web.DNS    as DNS
+import qualified Fission.Web.Heroku as Heroku
 import qualified Fission.Web.IPFS   as IPFS
 import qualified Fission.Web.Ping   as Ping
-import qualified Fission.Web.Heroku as Heroku
-import qualified Fission.Web.DNS    as DNS
-import qualified Fission.Web.Auth   as Auth
 import qualified Fission.Web.User   as User
-import qualified Fission.Web.User        as User
-import qualified Fission.Web.DNS         as DNS
-import qualified Fission.Web.Auth        as Auth
-
-import qualified Fission.Web.User        as User
 
 type API = IPFSRoute
       :<|> HerokuRoute
