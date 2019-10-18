7e27fd6a84c39b6d0692d8dd0cc31a5d4b5f0874
Ben Church <ben.douglas.church@gmail.com>
2019-10-18 12:49:09 +0200

    Implement liftE

--- library/Fission/CLI/Pin.hs
+++ library/Fission/CLI/Pin.hs
@@ -15,6 +15,7 @@ import Servant.Client
 import qualified Fission.Config as Config
 import           Fission.Internal.Constraint
 
+import           Fission.Internal.Exception
 import qualified Fission.IPFS.Peer    as IPFS.Peer
 import qualified Fission.IPFS.Types   as IPFS
 import           Fission.IPFS.CID.Types
@@ -28,6 +29,12 @@ import           Fission.CLI.Display.Success as CLI.Success
 import           Fission.CLI.Config.Types
 import RIO.List (headMaybe)
 
+maybeToEither:: Maybe a -> b -> Either a b
+maybeToEither a err = case a of
+  Just b -> Right b
+  Nothing -> Left err
+
+
 run :: MonadRIO          cfg m
     => HasLogFunc        cfg
     => HasProcessContext cfg
@@ -37,24 +44,33 @@ run :: MonadRIO          cfg m
     => CID
     -> UserConfig
     -> m (Either ClientError CID)
-run cid@(CID hash) userConfig = do
-  logDebug $ "Remote pinning " <> display hash
+run cid@(CID hash) userConfig = handleWith_ CLI.Error.put' $ do
+  maybePeer <- headMaybe $ peers userConfig
+  peer  <- liftE $ maybeToEither maybePeer "ERROR"
+  result  <- liftE $ IPFS.Peer.connect peer
+  Client.Runner runner <- Config.get
+  hold <- liftIO (pin runner userConfig cid)
+  CLI.Success.live hash
+  return $ Right cid
+
+  -- logDebug $ "Remote pinning " <> display hash
+
   -- IPFS.Peer.connect IPFS.Peer.fission
 
-  let hold = headMaybe $ peers userConfig
+  -- let hold = headMaybe $ peers userConfig
 
-  case hold of
-    Just pr -> IPFS.Peer.connect pr
+  -- case hold of
+  --   Just pr -> IPFS.Peer.connect pr
 
-  Client.Runner runner <- Config.get
-  liftIO (pin runner userConfig cid) >>= \case
-    Right _ -> do
-      CLI.Success.live hash
-      return $ Right cid
+  -- Client.Runner runner <- Config.get
+  -- liftIO (pin runner userConfig cid) >>= \case
+  --   Right _ -> do
+  --     CLI.Success.live hash
+  --     return $ Right cid
 
-    Left err -> do
-      CLI.Error.put' err
-      return $ Left err
+  --   Left err -> do
+  --     CLI.Error.put' err
+  --     return $ Left err
 
 pin :: MonadUnliftIO m => (ClientM NoContent -> m a) -> UserConfig -> CID -> m a
 pin runner userConfig cid = do
