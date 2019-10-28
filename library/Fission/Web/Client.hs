88a82ab11d246544814e6c0c7e7b8fce82f6f8a2
Ben Church <ben.douglas.church@gmail.com>
2019-10-28 16:20:26 -0600

    Remove unused client files

--- library/Fission/Web/Client.hs
+++
@@ -1,7 +0,0 @@
-module Fission.Web.Client
-  ( module Fission.Web.Client.Request
-  , module Fission.Web.Client.Types
-  ) where
-
-import Fission.Web.Client.Request
-import Fission.Web.Client.Types
