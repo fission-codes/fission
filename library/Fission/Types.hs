72fdccd90b3c36d9c661748b6b0d64aa9a71f779
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-11-11 17:14:26 +0100

    Remove dead file

--- library/Fission/Types.hs
+++
@@ -1,9 +0,0 @@
--- | Top level types
-module Fission.Types where -- (Fission) where
-
-import RIO
-
--- import Fission.Config.Types
-
--- | Top-level application type
--- type Fission be = RIO (Config be)
