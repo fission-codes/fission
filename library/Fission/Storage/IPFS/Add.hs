5bee793d971844d3b8189975d229fd01f5bfd3f4
Daniel Holmgren <dtholmgren@gmail.com>
2019-11-20 12:11:15 -0600

    rewrite walkDIr to use foldM

--- library/Fission/Storage/IPFS/Add.hs
+++ library/Fission/Storage/IPFS/Add.hs
@@ -15,7 +15,7 @@ import           RIO.Directory
 import           RIO.FilePath
 import qualified RIO.ByteString.Lazy as Lazy
 
-import           Fission.Prelude
+import           Fission.Prelude hiding (link)
 import           Fission.Internal.Process
 import qualified Fission.Internal.UTF8 as UTF8
 
@@ -144,24 +144,35 @@ walkDir ::
 walkDir ignored path = do
   files <- listDirectory path
   let toAdd = removeIgnored ignored files
-  results <- mapM (addDir ignored . combine path) toAdd 
-  case lefts results of
-    [] -> do
-      let names = IPFS.Name <$> toAdd
-      links <- zipWithM DAG.Link.create (rights results) names
-      case lefts links of
-        [] -> DAG.putNode Node
-              { dataBlock = "CAE="
-              , links = rights links
-              }
-        _  -> return . Left <| firstErr links
-    _  -> return . Left <| firstErr results
+  let f = foldResults path ignored
+  foldM f (Right <| Node { dataBlock = "CAE=", links = [] }) toAdd >>= \case
+    Left err -> return <| Left err
+    Right node -> DAG.putNode node
+
+foldResults ::
+  ( MonadRIO cfg m
+  , HasProcessContext cfg
+  , HasLogFunc cfg
+  , Has IPFS.BinPath cfg
+  , Has IPFS.Timeout cfg
+  )
+  => FilePath
+  -> IPFS.Ignored
+  -> Either IPFS.Error.Add Node
+  -> FilePath
+  -> m (Either IPFS.Error.Add Node)
+foldResults _ _ (Left err) _ = return <| Left err
+foldResults path ignored (Right node) filename = do
+  addDir ignored (path </> filename) >>= \case
+    Left err ->  return <| Left err
+    Right cid ->
+      DAG.Link.create cid (IPFS.Name filename) >>= \case
+      Left err -> return <| Left err
+      Right link ->
+        return <| Right <| node { links = link:(links node) }
 
 removeIgnored :: [Glob.Pattern] -> [FilePath] -> [FilePath]
 removeIgnored ignored files = List.filter (not . matchesAny ignored) files
 
 matchesAny :: [Glob.Pattern] -> FilePath -> Bool
 matchesAny globs path = List.any (\x -> Glob.match x path) globs
-              
-firstErr :: [Either a b] -> a
-firstErr = List.head . lefts
