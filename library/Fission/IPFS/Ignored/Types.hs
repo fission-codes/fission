4be947613d59b74b9724e16cb2b6a51a076d36be
Daniel Holmgren <dtholmgren@gmail.com>
2019-11-19 21:52:23 -0600

    got types wrong

--- library/Fission/IPFS/Ignored/Types.hs
+++ library/Fission/IPFS/Ignored/Types.hs
@@ -1,6 +1,5 @@
-module Fission.IPFS.Ignored.Types (Ignored (..)) where
+module Fission.IPFS.Ignored.Types (Ignored) where
 
-import           Fission.Prelude
 import qualified System.FilePath.Glob as Glob
 
 type Ignored = [Glob.Pattern]
--- library/Fission/IPFS/Types.hs
+++ library/Fission/IPFS/Types.hs
@@ -11,9 +11,7 @@ module Fission.IPFS.Types
   , Tag (..)
   , Timeout (..)
   , URL (..)
-  , Link (..)
-  , Node (..)
-  , Ignored (..)
+  , Ignored
   ) where
 
 import Fission.IPFS.BinPath.Types
@@ -25,6 +23,4 @@ import Fission.IPFS.Process.Types
 import Fission.IPFS.SparseTree.Types
 import Fission.IPFS.Timeout.Types
 import Fission.IPFS.URL.Types
-import Fission.IPFS.DAG.Link.Types
-import Fission.IPFS.DAG.Node.Types
 import Fission.IPFS.Ignored.Types
