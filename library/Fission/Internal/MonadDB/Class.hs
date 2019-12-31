e379435b34b3342247562dd73e51c43b19411d3e
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-12-30 16:35:44 -0800

    MonadAWS
    ice and cleaned up
    
    Cleaning up
    
    Cleaning up
    
    Cleaning up
    
    Cleaning up
    
    Cleaning up
    
    Cleaning up
    
    MonadHerokuAddOn
    
    Remove const from codebase
    
    MonadLinkedIPFS
    
    Add note
    
    Renamed dir
    
    Rename module
    
    Make pretty
    
    RIO -> Fission
    
    Remove Orphanage.RIO
    
    Rename module
    
    Rename module
    
    NumericUnderscores
    
    Remove dead code

--- library/Fission/Internal/MonadDB/Class.hs
+++ library/Fission/Internal/MonadDB/Class.hs
@@ -1,10 +1,4 @@
-{-# LANGUAGE UndecidableInstances #-}
-
 module Fission.Internal.MonadDB.Class (MonadDB (..)) where
-
-import Data.Has
-import Data.Pool
-import Database.Persist.Sql as Sql
 
 import RIO
 
@@ -12,8 +6,3 @@ import Fission.Internal.MonadDB.Types
 
 class MonadIO m => MonadDB m where
   runDB :: Transaction m a -> m a
-
-instance Has (Pool SqlBackend) cfg => MonadDB (RIO cfg) where
-  runDB transaction = do
-    pool <- view hasLens
-    runSqlPool transaction pool
