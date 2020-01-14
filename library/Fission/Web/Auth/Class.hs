cfa1aa016810379c98b7fd316e3fce5b30d4dfd7
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-01-13 16:59:53 -0800

    Comment

--- library/Fission/Web/Auth/Class.hs
+++ library/Fission/Web/Auth/Class.hs
@@ -4,4 +4,5 @@ import           Servant
 import           Fission.Prelude
 
 class Monad m => MonadAuth who m where
+  -- | Check that some entity is authenticated and authorized
   verify :: m (BasicAuthCheck who)
