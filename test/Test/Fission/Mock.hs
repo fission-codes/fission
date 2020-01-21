e2fb4af8aebe02f684d4ae12c56067f2a8114b08
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-01-20 17:04:30 -0800

    Cleanup

--- test/Test/Fission/Mock.hs
+++ test/Test/Fission/Mock.hs
@@ -4,29 +4,23 @@ module Test.Fission.Mock
   , runMock
   ) where
 
-import Test.Fission.Mock.Types
-import Test.Fission.Mock.Effect
-
-
 import           Control.Monad.Writer
 import           Data.Generics.Product
 import qualified Network.IPFS.Types  as IPFS
 import           Servant
 
+import           Test.Hspec.Core.QuickCheck (modifyMaxSize)
+import           Test.QuickCheck
 import           Test.Tasty.Hspec
+
+import           Test.Fission.Mock.Types
+import           Test.Fission.Mock.Effect
 
 import           Fission.IPFS.Linked.Class
 import           Fission.Prelude
 import           Fission.Web.Auth.Class
 
-
-
-
-
-
-import Test.Hspec.Core.QuickCheck (modifyMaxSize)
-import Test.QuickCheck
-
+-- | Run the action described by a @Mock@
 runMock :: MonadIO m => ctx -> Mock effs ctx a -> m (MockSession effs a)
 runMock ctx action =
   action
