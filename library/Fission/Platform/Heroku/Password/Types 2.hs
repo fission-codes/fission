1bcade278a2cb9ff2545b329f583d629d8e6a277
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-02-12 11:07:30 -0700

    Remove duplicate files

--- library/Fission/Platform/Heroku/Password/Types 2.hs
+++
@@ -1,8 +0,0 @@
-module Fission.Platform.Heroku.Password.Types (Password (..)) where
-
-import Fission.Prelude
-
--- | Heroku add-on password (from @addon-manifest.json@)
-newtype Password = Password { getPassword :: ByteString }
-  deriving         (Eq, Show)
-  deriving newtype IsString
