69b07ddaf79bd45fd8e07706628bd9e6d00c5f5f
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-02-26 11:23:56 -0800

    Works again

--- library/Fission/Web/App/Destroy.hs
+++ library/Fission/Web/App/Destroy.hs
@@ -1,6 +1,10 @@
 module Fission.Web.App.Destroy
   ( API
-  , destroy
+  , ByURLAPI
+  , ByIdAPI
+  , server
+  , destroyById
+  , destroyByURL
   ) where
 
 import           Database.Esqueleto
@@ -13,11 +17,18 @@ import           Fission.URL.Types
 import qualified Fission.App.Destroyer.Class as App
 import           Fission.Web.Error.Class
 
-type API
-  =  Capture "url" URL
+type API = ByURLAPI :<|> ByIdAPI
+
+type ByURLAPI
+  =  "associated"
+  :> Capture "url" URL
   :> DeleteNoContent '[JSON] NoContent
 
-destroy ::
+type ByIdAPI
+  =  Capture "appId" AppId
+  :> DeleteNoContent '[JSON] NoContent
+
+server ::
   ( MonadTime       m
   , MonadThrow      m
   , MonadDB       t m
@@ -25,7 +36,30 @@ destroy ::
   )
   => Entity User
   -> ServerT API m
-destroy (Entity userId _) URL {..} =
+server user = destroyByURL user :<|> destroyById user
+
+destroyByURL ::
+  ( MonadTime       m
+  , MonadThrow      m
+  , MonadDB       t m
+  , App.Destroyer t
+  )
+  => Entity User
+  -> ServerT ByURLAPI m
+destroyByURL (Entity userId _) URL {..} =
   runDBNow (App.destroyByURL userId domainName subdomain) >>= \case
+    Right () -> return NoContent
+    Left err -> throwM <| toServerError err
+
+destroyById ::
+  ( MonadTime       m
+  , MonadThrow      m
+  , MonadDB       t m
+  , App.Destroyer t
+  )
+  => Entity User
+  -> ServerT ByIdAPI m
+destroyById (Entity userId _) appId =
+  runDBNow (App.destroy userId appId) >>= \case
     Right () -> return NoContent
     Left err -> throwM <| toServerError err
