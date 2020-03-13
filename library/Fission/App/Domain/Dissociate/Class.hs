1ebb6e322c9a88b18ee8f2ba426d980cab8c2318
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-03-13 14:49:19 -0700

    Clean up commented-out code

--- library/Fission/App/Domain/Dissociate/Class.hs
+++ library/Fission/App/Domain/Dissociate/Class.hs
@@ -4,7 +4,6 @@ module Fission.App.Domain.Dissociate.Class
   ) where
 
 import           Database.Esqueleto
--- import qualified Database.Persist as P
 
 import           Fission.Prelude
 import           Fission.Models
@@ -31,11 +30,6 @@ class Monad m => Dissociate m where
     -> Maybe Subdomain
     -> UTCTime
     -> m (Either Errors ())
-
-  -- dissociateAll :: AppId -> UTCTime -> m (Either Errors ())
-
-  -- FIXME: move to Domain/Delete
-  -- dissociateAllByDomainName :: AppId -> DomainName -> UTCTime -> m (Either Errors ())
 
 instance MonadIO m => Dissociate (Transaction m) where
   dissociate userId appId domainName maySubdomain now =
@@ -64,25 +58,3 @@ instance MonadIO m => Dissociate (Transaction m) where
             return case howMany of
               0 -> Error.openLeft <| DissociateDomain.NotRegisteredToApp appId domainName maySubdomain
               _ -> Right ()
-
-  -- dissociateAllByDomainName appId domainName now = do
-  --   appDomains <- select <| from \appDomain -> do
-  --     where_ <|  appDomain ^. AppDomainAppId      ==. val appId
-  --            &&. appDomain ^. AppDomainDomainName ==. val domainName
-  --     return appDomain
-
-  --   case appDomains of
-  --     [] ->
-  --       return . Error.openLeft <| DissociateDomain.NotRegisteredToApp appId domainName Nothing
-
-  --     _ -> do
-  --       forM_ appDomains \(Entity appDomainId AppDomain {..}) -> do
-  --         P.delete appDomainId
-  --         insert_ DissociateAppDomainEvent
-  --           { dissociateAppDomainEventAppId      = appDomainAppId
-  --           , dissociateAppDomainEventDomainName = appDomainDomainName
-  --           , dissociateAppDomainEventSubdomain  = appDomainSubdomain
-  --           , dissociateAppDomainEventInsertedAt = now
-  --           }
-
-  --       return <| Right ()
