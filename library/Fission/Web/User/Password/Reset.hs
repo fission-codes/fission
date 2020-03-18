13ccd71ecf6ede102cafeb952ef85524df9f57c7
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-03-18 00:40:54 -0700

    Fix Put -> Patch

--- library/Fission/Web/User/Password/Reset.hs
+++ library/Fission/Web/User/Password/Reset.hs
@@ -18,7 +18,7 @@ import qualified Fission.Web.User.Password.Reset.Types as User.Password
 type API
   =  Summary "Reset password"
   :> ReqBody '[JSON] User.Password.Reset
-  :> Put     '[JSON] User.Password
+  :> Patch   '[JSON] User.Password
 
 server ::
   ( MonadThrow      m
