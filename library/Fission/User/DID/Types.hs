27f0ce1b7385450b4744736f0b8d524eb8b442e5
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-04-02 10:03:41 -0700

    Reformat giant comment

--- library/Fission/User/DID/Types.hs
+++ library/Fission/User/DID/Types.hs
@@ -17,36 +17,37 @@ import           Fission.Key as Key
 import           Fission.User.DID.Method.Types
 import qualified Fission.Internal.UTF8 as UTF8
 
--- | A DID key, broken into its constituant parts
---
--- Format:
--- did:key:<MULTIBASE(base58-btc, MULTICODEC(public-key-type, raw-public-key-bytes))>
---
--- > NOTE: Multibase-prefixes are encoding agnostic. "z" is "z",
--- > not 0x7a ("z" encoded as ASCII/UTF-8).
--- > For example, in UTF-32, "z" would be [0x7a, 0x00, 0x00, 0x00].
--- >
--- > Expressing a base58btc encoded ed25519 cryptographic identifier would look like this:
--- > 0x7a 0xed01 ED25519_PUBLIC_KEY_BYTES
--- > [...]
--- > Expressing a cryptographic identifier that is a base58btc encoded RSA SPKI-based
--- >   public key fingerprint using SHA2-256/256 would look like this:
--- > 0x7a 0x5d 0x12 0x20 HASH_BYTES
--- https://github.com/w3c-ccg/lds-ed25519-2018/issues/3
---
--- > So the procedure to express other types of keys is:
--- >
--- >    1. Find the multicodec representation for your key type.
--- >         For example, the registration for secp256k1 keys is pending
--- >         over at multiformats/multicodec#160.
--- >    2. Encode the raw public key bytes using that multicodec value.
--- >    3. Pick a character encoding (I suggest base58-btc),
--- >         and convert it to a character string using Multibase.
--- >    4. Prepend did:key: to that string, and there you have it!
--- https://github.com/w3c-ccg/did-method-key/issues/3#issuecomment-594190524
---
--- NOTE: The multihash is in little-endian base-7 with continuation flags
--- More here: https://github.com/multiformats/unsigned-varint
+{- | A DID key, broken into its constituant parts
+
+Format:
+did:key:<MULTIBASE(base58-btc, MULTICODEC(public-key-type, raw-public-key-bytes))>
+
+> NOTE: Multibase-prefixes are encoding agnostic. "z" is "z",
+> not 0x7a ("z" encoded as ASCII/UTF-8).
+> For example, in UTF-32, "z" would be [0x7a, 0x00, 0x00, 0x00].
+>
+> Expressing a base58btc encoded ed25519 cryptographic identifier would look like this:
+> 0x7a 0xed01 ED25519_PUBLIC_KEY_BYTES
+> [...]
+> Expressing a cryptographic identifier that is a base58btc encoded RSA SPKI-based
+>   public key fingerprint using SHA2-256/256 would look like this:
+> 0x7a 0x5d 0x12 0x20 HASH_BYTES
+https://github.com/w3c-ccg/lds-ed25519-2018/issues/3
+
+> So the procedure to express other types of keys is:
+>
+>    1. Find the multicodec representation for your key type.
+>         For example, the registration for secp256k1 keys is pending
+>         over at multiformats/multicodec#160.
+>    2. Encode the raw public key bytes using that multicodec value.
+>    3. Pick a character encoding (I suggest base58-btc),
+>         and convert it to a character string using Multibase.
+>    4. Prepend did:key: to that string, and there you have it!
+https://github.com/w3c-ccg/did-method-key/issues/3#issuecomment-594190524
+
+NOTE: The multihash is in little-endian base-7 with continuation flags
+More here: https://github.com/multiformats/unsigned-varint
+-}
 data DID = DID
   { publicKey :: !Key.Public
   , algorithm :: !Key.Algorithm
