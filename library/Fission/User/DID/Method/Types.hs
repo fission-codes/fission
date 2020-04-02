0b62ef945080c35806b472dab51a28488625c609
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-04-02 00:03:10 -0700

    Fix issue in base58 deserializer

---
+++ library/Fission/User/DID/Method/Types.hs
@@ -0,0 +1,18 @@
+module Fission.User.DID.Method.Types (Method (..)) where
+
+import           Fission.Prelude
+
+data Method
+  = Key
+  deriving (Show, Eq)
+
+instance Display Method where
+  display Key = "key"
+
+instance ToJSON Method where
+  toJSON Key = "key"
+
+instance FromJSON Method where
+  parseJSON = withText "DID.Method" \case
+    "key" -> return Key
+    other -> fail $ show other <> " is not an acceptable DID method"
--- library/Fission/User/DID/Types.hs
+++ library/Fission/User/DID/Types.hs
@@ -1,18 +1,20 @@
--- | 
-
 module Fission.User.DID.Types
-  ( DID    (..)
-  , Method (..)
+  ( DID (..)
+  -- * Reexport
+  , module Fission.User.DID.Method.Types
   ) where
 
 import           Data.Binary
 import           Data.Base58String.Bitcoin as BS58.BTC
 
-import qualified RIO.ByteString as BS
-import qualified RIO.Text       as Text
+import qualified RIO.ByteString      as BS
+import qualified RIO.ByteString.Lazy as BS.Lazy
+import qualified RIO.Text            as Text
 
 import           Fission.Prelude
 import           Fission.Key as Key
+ 
+import           Fission.User.DID.Method.Types
 
 -- | A DID key, broken into its constituant parts
 --
@@ -43,7 +45,7 @@ import           Fission.Key as Key
 -- https://github.com/w3c-ccg/did-method-key/issues/3#issuecomment-594190524
 --
 -- NOTE: The multihash is in little-endian base-7 with continuation flags
--- More here:https://github.com/multiformats/unsigned-varint
+-- More here: https://github.com/multiformats/unsigned-varint
 data DID = DID
   { publicKey :: !Key.Public
   , algorithm :: !Key.Algorithm
@@ -51,73 +53,53 @@ data DID = DID
   } deriving (Show, Eq)
 
 instance ToJSON DID where
-  toJSON (DID pk algo method) = String (header <> keyType <> pk58)
+  toJSON (DID (Key.Public pk) algo method) =
+    String (header <> toBase58Text multicodec64)
     where
       header :: Text
       header = "did:" <> textDisplay method <> ":" <> "z"
 
-      keyType :: Text
-      keyType = toBase58Text keyTypeRaw
+      multicodec64 :: ByteString
+      multicodec64 = BS.pack magicBytes <> encodeUtf8 pk
 
-      keyTypeRaw :: [Word8]
-      keyTypeRaw =
+      magicBytes :: [Word8]
+      magicBytes =
         case algo of
           Ed25519 -> [0xed, 0x01]
           RSA2048 -> [0x00, 0x75, 0x01]
-
-      pk58 :: Text
-      pk58 = toBase58Text pk
-
-      toBase58Text :: Binary a => a -> Text
-      toBase58Text = decodeUtf8Lenient . BS58.BTC.toBytes . BS58.BTC.fromBinary
-
--- b1111010100000001 = F501 -- FIXME PLEASE DOUBLE CHECK THE MATH
-
--- FIXME: THIS NEED A BUUUUUNCH OF TESTS (confusing encodnig)
 
 instance FromJSON DID where
   parseJSON = withText "DID" \txt ->
     case Text.stripPrefix "did:key:z" txt of
       Nothing ->
-        error $ show txt <> " does not have a valid did:key header"
+        fail $ show txt <> " does not have a valid did:key header"
 
       Just fragment ->
-        fragment
-          |> BS58.BTC.fromText
-          |> BS58.BTC.toBytes
-          |> BS.unpack
-          |> \case
-            (0xed : 0x01 : edKeyW8s) ->
-              return DID
-                { publicKey = Key.Public $ bytesToText edKeyW8s
-                , algorithm = Ed25519
-                , method    = Key
-                }
+        case decode' . BS.Lazy.fromStrict . encodeUtf8 $ fragment of
+          Nothing ->
+            fail $ show fragment <> " is not a properly formatted multicodec"
+           
+          Just b58 ->
+            case BS58.BTC.toBinary b58 of
+              (0xed : 0x01 : edKeyW8s) ->
+                return DID
+                  { publicKey = Key.Public $ bytesToText edKeyW8s
+                  , algorithm = Ed25519
+                  , method    = Key
+                  }
 
-            (0x00 : 0x75 : 0x01 : rsaKeyW8s) ->
-              return DID
-                { publicKey = Key.Public $ bytesToText rsaKeyW8s
-                , algorithm = RSA2048
-                , method    = Key
-                }
+              (0x00 : 0x75 : 0x01 : rsaKeyW8s) ->
+                return DID
+                  { publicKey = Key.Public $ bytesToText rsaKeyW8s
+                  , algorithm = RSA2048
+                  , method    = Key
+                  }
 
-            nope ->
-              fail $ show nope <> " is not an acceptable did:key"
-    where
-      bytesToText :: [Word8] -> Text
-      bytesToText = decodeUtf8Lenient . BS.pack
+              nope ->
+                fail $ show nope <> " is not an acceptable did:key"
 
-data Method
-  = Key
-  deriving (Show, Eq)
+bytesToText :: [Word8] -> Text
+bytesToText = decodeUtf8Lenient . BS.pack
 
-instance Display Method where
-  display Key = "key"
-
-instance ToJSON Method where
-  toJSON Key = "key"
-
-instance FromJSON Method where
-  parseJSON = withText "DID.Method" \case
-    "key" -> return Key
-    other -> fail $ show other <> " is not an acceptable DID method"
+toBase58Text :: Binary a => a -> Text
+toBase58Text bin = decodeUtf8Lenient . BS58.BTC.toBytes $ BS58.BTC.fromBinary bin
