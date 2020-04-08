a2987bc988de12ed88b196dc395bc91588db235f
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-04-07 20:34:28 -0700

    moar cleanup

--- library/Fission/Web/Auth/JWT/Signature/RS256/Types.hs
+++ library/Fission/Web/Auth/JWT/Signature/RS256/Types.hs
@@ -1,10 +1,10 @@
 module Fission.Web.Auth.JWT.Signature.RS256.Types (Signature (..)) where
 
 import           Data.ByteArray
-import qualified Data.ByteString.Base64 as Base64
-import qualified RIO.Text.Partial       as PText
+import qualified Data.ByteString.Base64 as B64
 
 import           Fission.Prelude hiding (length)
+import qualified Fission.Internal.Base64.URL as B64.URL
 
 newtype Signature = Signature { unSignature :: ByteString }
   deriving (Eq, Show)
@@ -14,7 +14,7 @@ instance Arbitrary Signature where
 
 instance FromJSON Signature where
   parseJSON = withText "RS256Siganture" \txt ->
-    return . Signature . Base64.decodeLenient . encodeUtf8 $ fromURLEncoding txt
+    pure . Signature . B64.decodeLenient . encodeUtf8 $ B64.URL.decode txt
 
 instance ToJSON Signature where
   toJSON = String . decodeUtf8Lenient . unSignature
@@ -22,7 +22,3 @@ instance ToJSON Signature where
 instance ByteArrayAccess Signature where
   length        = length        . unSignature
   withByteArray = withByteArray . unSignature
-
-  -- FIXME move
-fromURLEncoding :: Text -> Text
-fromURLEncoding = PText.replace "-" "+" . PText.replace "_" "/"
