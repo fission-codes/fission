f11afe896376500c7dd0bbb0e9c92ff4c75d5734
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-04-09 00:27:18 -0700

    WIP

--- library/Fission/Internal/Crypto.hs
+++ library/Fission/Internal/Crypto.hs
@@ -8,8 +8,7 @@ import qualified Data.ASN1.BinaryEncoding as ASN1
 import qualified Data.ASN1.Encoding       as ASN1
 import qualified Data.ASN1.Types          as ASN1
  
-import qualified Data.X509                 as X509
--- import qualified Data.Base58String.Bitcoin as BS58.BTC
+import qualified Data.X509 as X509
 
 import           Crypto.Error
 import qualified Crypto.PubKey.Ed25519   as Crypto.Ed25519
@@ -17,13 +16,6 @@ import qualified Crypto.PubKey.RSA.Types as Crypto.RSA
 
 import           Fission.Prelude
 import           Fission.Internal.Base64.Scrubbed as B64.Scrubbed
-
-
-
-
-
--- import Data.ASN1.Prim
--- import Crypto.Number.Serialize
 
 base64ToEd25519Signature :: ByteString -> CryptoFailable Crypto.Ed25519.Signature
 base64ToEd25519Signature = Crypto.Ed25519.signature . B64.Scrubbed.scrubB64
@@ -36,4 +28,4 @@ decodeToRSA2048PK :: ByteString -> Either String Crypto.RSA.PublicKey
 decodeToRSA2048PK raw =
   case ASN1.fromASN1 <$> ASN1.decodeASN1' ASN1.DER raw of
     Right (Right (X509.PubKeyRSA pk, _)) -> Right pk
-    err -> Left $ "Unable to decode as RSA 2048 key: " <> show err -- raw
+    _ -> Left $ "Unable to decode as RSA 2048 key: " <> show raw
--- library/Fission/Web/Auth/JWT/Validation.hs
+++ library/Fission/Web/Auth/JWT/Validation.hs
@@ -38,7 +38,7 @@ check jwt = check' jwt <$> currentTime
 check' :: JWT -> UTCTime -> Either JWT.Error JWT
 check' jwt now =
   pure jwt
-    >>= checkTime now
+--     >>= checkTime now
     >>= checkSignature
 
 checkTime :: UTCTime -> JWT -> Either JWT.Error JWT
@@ -61,15 +61,24 @@ checkRSA2048Signature jwt@JWT {..} (RS256.Signature innerSig) = do
       Left BadPublicKey
 
     Right pk ->
-      if Crypto.RSA.PKCS.verify (Just SHA256) pk content innerSig
+      if Crypto.RSA.PKCS.verify (Nothing :: Maybe SHA256) pk content innerSig
+      -- if Crypto.RSA.PKCS.verify (Just SHA256) pk content innerSig
         then Right jwt
         else Left IncorrectSignature
  
   where
     Claims {iss = User.DID {publicKey = Key.Public pk'}} = claims
-    content =
-      (encodeUtf8 $ B64.URL.encode $ decodeUtf8Lenient $ B64.toB64ByteString $ Lazy.toStrict (encode header)) <> "."
-        <> (encodeUtf8 $ B64.URL.encode $ decodeUtf8Lenient $ B64.toB64ByteString $ Lazy.toStrict $ encode claims)
+    -- content = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODY0MjAwOTAsImlzcyI6ImRpZDprZXk6ejEzVjNTb2cyWWFVS2hkR0NtZ3g5VVp1VzFvMVNoRkpZYzZEdkdZZTdOVHQ2ODlOb0wzZkF2TlhDQkhNbnVab2JVVm9yVGtQZ1FBbVl3TmdQS1pGZVp0Z0Z0OE1nUWNweGFXVFV4NjVocUtmcHVSd3JqZnFHSlRMbmk1dnoxZzg1RWlDdUhLeW9XMmp1cW54VXRIOUZkV0JVR0VwVWlvTk1CNFZydk1GWlE4VHpSQmJ1SlZyS3pFMURhVWdvYTdqcnM5bU1Ubmh1Y1h4NW1uOWE0aVlEU0xTZHI2bzVuckQ4c0VEbllmdWNYc0w4dHVNQjlhaUhjYWlidE1Ya3oxUmlXRXNndzhTOTliWjgxQmNuM3l2ZTJHOUtrU3F0WHNlNmZTaXJXaTFyS2hUMUxSQ0VCSnhYbkFxY25NZlQ0cU5ORE5USlUzb2VWUmVWYzJ6bXhhZUM4aGNIWXAzRjdnNWtjclJONEdvc2lQdnpWVkdhNXZDTnBrNHc4OHJkTVZiek5uanJVWlNhRmY3RVRmWmhRd25MQ0ciLCJqdGkiOiI1ZjBiN2RhZi1lOTZkLTQyZWItYmE1ZC0yZTVlNjExZTQ3ZWMiLCJpYXQiOjE1ODY0MTYyNzB9"
+  --   content = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODY0MjA0MDgsImlzcyI6ImRpZDprZXk6ejEzVjNTb2cyWWFVS2hkR0NtZ3g5VVp1VzFvMVNoRkpZYzZEdkdZZTdOVHQ2ODlOb0wzZkF2TlhDQkhNbnVab2JVVm9yVGtQZ1FBbVl3TmdQS1pGZVp0Z0Z0OE1nUWNweGFXVFV4NjVocUtmcHVSd3JqZnFHSlRMbmk1dnoxZzg1RWlDdUhLeW9XMmp1cW54VXRIOUZkV0JVR0VwVWlvTk1CNFZydk1GWlE4VHpSQmJ1SlZyS3pFMURhVWdvYTdqcnM5bU1Ubmh1Y1h4NW1uOWE0aVlEU0xTZHI2bzVuckQ4c0VEbllmdWNYc0w4dHVNQjlhaUhjYWlidE1Ya3oxUmlXRXNndzhTOTliWjgxQmNuM3l2ZTJHOUtrU3F0WHNlNmZTaXJXaTFyS2hUMUxSQ0VCSnhYbkFxY25NZlQ0cU5ORE5USlUzb2VWUmVWYzJ6bXhhZUM4aGNIWXAzRjdnNWtjclJONEdvc2lQdnpWVkdhNXZDTnBrNHc4OHJkTVZiek5uanJVWlNhRmY3RVRmWmhRd25MQ0ciLCJqdGkiOiJkZGQ3MjQ5OS0xZmZmLTRhOTMtYTUxMy00YjBiYjQwNTYwNGIiLCJpYXQiOjE1ODY0MTY2MDZ9"
+    content = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODYzNzEzNDkxMjYsImlzcyI6ImRpZDprZXk6ejEzVjNTb2cyWWFVS2hkR0NtZ3g5VVp1VzFvMVNoRkpZYzZEdkdZZTdOVHQ2ODlOb0wzZkF2TlhDQkhNbnVab2JVVm9yVGtQZ1FBbVl3TmdQS1pGZVp0Z0Z0OE1nUWNweGFXVFV4NjVocUtmcHVSd3JqZnFHSlRMbmk1dnoxZzg1RWlDdUhLeW9XMmp1cW54VXRIOUZkV0JVR0VwVWlvTk1CNFZydk1GWlE4VHpSQmJ1SlZyS3pFMURhVWdvYTdqcnM5bU1Ubmh1Y1h4NW1uOWE0aVlEU0xTZHI2bzVuckQ4c0VEbllmdWNYc0w4dHVNQjlhaUhjYWlidE1Ya3oxUmlXRXNndzhTOTliWjgxQmNuM3l2ZTJHOUtrU3F0WHNlNmZTaXJXaTFyS2hUMUxSQ0VCSnhYbkFxY25NZlQ0cU5ORE5USlUzb2VWUmVWYzJ6bXhhZUM4aGNIWXAzRjdnNWtjclJONEdvc2lQdnpWVkdhNXZDTnBrNHc4OHJkTVZiek5uanJVWlNhRmY3RVRmWmhRd25MQ0ciLCJuYmYiOm51bGx9" -- "kHGfUlB-pog86ON5loYsLGPydcRvxk49ez-AI17ko6Bj8OBdHqnSwggHl3NMy9DCEg4N0Xkmp2X_-E4gnlJ9hCNEDJB2v61sbQCvqVJZtrF2RGAk8ZyCQLA5dHuRTvb8jhI453C-_ocnkH0Q-42Z8jI_JkeghauRqJanudBJQOcX2MbRrt84zYZ7eATRM-sXUdy2NtaGPwP2i5h2iNOZGlSnj-GsnFkzIBSPHBw2Wm1p-1MJ2DM_smWx_b_NfE379KsvDIRPZ0y_3ZPO3tCWAi9t69jls1_RKD5EdwkKFTnGbCWaHLdF-78-YvTJcQ2fsj4e5xD-Ok6shQvrkkLt5A"
+    -- content = "kHGfUlB-pog86ON5loYsLGPydcRvxk49ez-AI17ko6Bj8OBdHqnSwggHl3NMy9DCEg4N0Xkmp2X_-E4gnlJ9hCNEDJB2v61sbQCvqVJZtrF2RGAk8ZyCQLA5dHuRTvb8jhI453C-_ocnkH0Q-42Z8jI_JkeghauRqJanudBJQOcX2MbRrt84zYZ7eATRM-sXUdy2NtaGPwP2i5h2iNOZGlSnj-GsnFkzIBSPHBw2Wm1p-1MJ2DM_smWx_b_NfE379KsvDIRPZ0y_3ZPO3tCWAi9t69jls1_RKD5EdwkKFTnGbCWaHLdF-78-YvTJcQ2fsj4e5xD-Ok6shQvrkkLt5A"
+      -- encodeUtf8 . B64.URL.encode $ decodeUtf8Lenient $
+      --   UTF8.stripPadding (Lazy.toStrict (encode header)) <> "." <> UTF8.stripPadding (Lazy.toStrict (encode claims))
+    -- content =
+    --   (B64.toB64ByteString $ Lazy.toStrict (encode header)) <> "."
+    --     <> (B64.toB64ByteString $ Lazy.toStrict $ encode claims)
+    --   -- (encodeUtf8 $ B64.URL.encode $ decodeUtf8Lenient $ B64.toB64ByteString $ Lazy.toStrict (encode header)) <> "."
+    --   --   <> (encodeUtf8 $ B64.URL.encode $ decodeUtf8Lenient $ B64.toB64ByteString $ Lazy.toStrict $ encode claims)
 
 checkEd25519Signature :: JWT -> Either JWT.Error JWT
 checkEd25519Signature jwt@JWT {..} =
