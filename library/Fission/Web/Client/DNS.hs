bfa313e7ab055e00f5d794f1511d104060ad3561
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-04-21 14:34:08 -0700

    Remove dead file

--- library/Fission/Web/Client/DNS.hs
+++
@@ -1,9 +0,0 @@
-module Fission.Web.Client.DNS where -- (update) where
-
--- import           Fission.Prelude
--- import qualified Fission.Web.Routes as Routes
-
--- type Update = Routes.DNSRoute
-
--- update :: Proxy
--- update = Proxy
