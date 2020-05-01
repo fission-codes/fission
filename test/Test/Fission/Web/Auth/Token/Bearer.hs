fec7d992a85a6130ecba5b3ba3c02c8cd3aa36a1
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-05-01 16:30:52 -0700

    Cleanup

--- test/Test/Fission/Web/Auth/Token/Bearer.hs
+++ test/Test/Fission/Web/Auth/Token/Bearer.hs
@@ -25,10 +25,6 @@ tests =
           |> JSON.decode
           |> shouldBe (Just Bearer.tokenRSA2048)
 
-      -- it "can decode Steven's real world example" do
-      --   let ex = "\"Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsInVhdiI6IjAuMS4wIn0.eyJhdWQiOiJkaWQ6a2V5OnpTdEVacHpTTXRUdDlrMnZzemd2Q3dGNGZMUVFTeUExNVc1QVE0ejNBUjZCeDRlRko1Y3JKRmJ1R3hLbWJtYTQiLCJleHAiOjE1ODgyNjU0MjAsImlzcyI6ImRpZDprZXk6ejEzVjNTb2cyWWFVS2hkR0NtZ3g5VVp1VzFvMVNoRkpZYzZEdkdZZTdOVHQ2ODlOb0wyY0V3ckIzTHA0cWMyczVuUUJScmJSUkVwR0dyYzhZaFJncHRKNjZFRWZRdHhnWlR2ZDRuajY2dmk4aWVqWXIxS0xoaXM3YnRyVlRpQXd4MzR5VWt0eUFMQU1kYzlEVFFMOXhRam5aMVFQWm1jVjJwdVFZejMya21uSEdFV0Z3Tk5lTGhmS2Z6elpxWXdCdTdDc3drQ3pycHpkWXhkOTVKVHd2RGJFNjFMTUFwanE2dTlQNlBUdlhOOUpGaVphUU1hZ0hwa2VCd3JZdjhQMUpZcFBmc1k4NHVyMmtYQzRhcTRaSGt3NXpEb1BwTDNVV240cTZUcHpGMjQ4R01LVExBejJtRE5NZWJYTjNUdW5QV3dudTlDaG92VlQ1TFN4cHVyU21WQW55OE5EaldQSktRbnJickRyMUUzNVlUdERIUjVrdWduNm90RHVtUGZpVm9XMWltdlhMTTJEam1tdnhDYUVVdXYiLCJuYmYiOjE1ODgyNjUzMzAsInBjeSI6IkFQUEVORCIsInByZiI6bnVsbCwic2NwIjoiLyJ9.S87ewBXBPb_P5BksTLu1lX-aCA_K4MqdMl9i8EuIpfbI7tuf9XK42NI0G-9Z7R3qV6zlQexQxKPlny-QE-Zz0W7fhvvameaNK0Eh-f1HRwJgGxHTMc3xmO9sFeOaCQjxe4Xon6V66en6XCM0ZQZm2J7fwVb9vYL2JKkWpOs1FuaREBTIddtP-gChHQOjLzCzkC8ti2hPhdF133R4WnHY7zKzMt4US2ufp_QOai5idga1YqIB2M1SkUUy_azqB_rUTZ9riMkcKiSJlnVj6gs1Zm4IK-3Xfq4DjtqOF8P4sckt-2-u_ZIICMBdQppjT30lXdeuDlJcU5JxL9tzhIwWMw\""
-      --   JSON.eitherDecode ex `shouldBe` Right Bearer.tokenRSA2048
-
     describe "serialization" do
       itsProp' "serialize+deserialize is the identity function" \(bearer :: Bearer.Token) ->
         JSON.eitherDecode (JSON.encode bearer) `shouldBe` Right bearer
