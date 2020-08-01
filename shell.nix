d7d59bfb8ca4b571a9feaa22b4afd084ed3d5b1c
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-07-31 21:33:26 -0700

    Set up Niv (#366)

---
+++ shell.nix
@@ -0,0 +1,31 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs    = import sources.nixpkgs {};
+in
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.curl
+    pkgs.gnumake
+    pkgs.zlib
+
+    # Data
+    # pkgs.ipfs
+    pkgs.libpqxx
+    pkgs.postgresql
+
+    # Haskell
+    pkgs.ghcid
+    pkgs.stack
+    pkgs.stylish-haskell
+
+    # Fun
+    pkgs.figlet
+    pkgs.lolcat
+  ];
+
+  shellHook = ''
+    echo "Welcome to the"
+    ${pkgs.figlet}/bin/figlet "Fission Server Shell" | lolcat -a -s 50
+  '';
+}
