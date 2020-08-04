397d90fb4d9f8df2f7fd7ca0ee6a982bf1236a92
Daniel Holmgren <dtholmgren@gmail.com>
2020-08-04 16:33:57 -0500

    attempt peer connections in parallel

--- library/Fission/CLI/IPFS/Connect.hs
+++ library/Fission/CLI/IPFS/Connect.hs
@@ -8,6 +8,7 @@ import           Fission.Prelude
 
 import qualified System.Console.ANSI as ANSI
 import qualified RIO.NonEmpty        as NonEmpty
+import           Control.Parallel.Strategies (parMap, rpar)
 
 import           Fission.Web.Client
 import           Fission.Web.Client.Peers as Peers
@@ -19,7 +20,6 @@ import           Fission.CLI.IPFS.Error.Types
 import           Network.IPFS
 import qualified Network.IPFS.Types       as IPFS
 import qualified Network.IPFS.Peer        as IPFS.Peer
-import qualified Network.IPFS.Peer.Error  as IPFS.Peer
 
 
 -- | Connect to the Fission IPFS network with a set amount of retries
@@ -36,7 +36,7 @@ swarmConnectWithRetry _peers (-1) =
   return . Left $ toException UnableToConnect
 
 swarmConnectWithRetry peers tries = do
-  attempts <- connectToPeers $ NonEmpty.toList peers
+  attempts <- sequence $ parMap rpar IPFS.Peer.connect (NonEmpty.toList peers)
   if any isRight attempts
     then do
       logDebug $ "Successfully connected to a node. Full results: " <> textShow attempts
@@ -50,15 +50,6 @@ swarmConnectWithRetry peers tries = do
         Right retryPeers -> do
           UTF8.putText "🛰 Unable to connect to the Fission IPFS peer, trying again...\n"
           swarmConnectWithRetry retryPeers (tries - 1)
-
--- | Connect to a list of peers
-connectToPeers ::
-  ( MonadUnliftIO  m
-  , MonadLocalIPFS m
-  )
-  => [IPFS.Peer]
-  -> m [Either IPFS.Peer.Error ()]
-connectToPeers peers = sequence $ IPFS.Peer.connect <$> peers
 
 -- | Create a could not connect to Fission peer message for the terminal
 couldNotSwarmConnect :: MonadIO m => m ()
