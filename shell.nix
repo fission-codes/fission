ec163db159ec35fa8bf3b125cad3b5ae89b30d68
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-08-08 12:22:55 -0700

    Fix IPFS remote stat API (#379)

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 let
   sources = import ./nix/sources.nix;
   pkgs    = import sources.nixpkgs {};
+  hinotify = if pkgs.stdenv.isDarwin then pkgs.hfsevents else pkgs.hinotify;
 in
 
 pkgs.mkShell {
@@ -10,9 +11,11 @@ pkgs.mkShell {
     pkgs.zlib
 
     # Data
-    # pkgs.ipfs
-    pkgs.libpqxx
+    pkgs.ipfs
+    pkgs.haskellPackages.postgresql-libpq
     pkgs.postgresql
+
+    hinotify
 
     # Haskell
     pkgs.ghcid
