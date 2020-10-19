93f988be94cbfe97edb514befa607994b9c9b618
Brooklyn Zelenka <hello@brooklynzelenka.com>
2020-10-19 19:54:39 +0000

    cross compile
    Remove result/

--- default.nix
+++ default.nix
@@ -1,23 +1,12 @@
-{ # Fetch the latest haskell.nix and import its default.nix
-  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}
-
-# haskell.nix provides access to the nixpkgs pins which are used by our CI,
-# hence you will be more likely to get cache hits when using these.
-# But you can also just use your own, e.g. '<nixpkgs>'.
-, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
-
-# haskell.nix provides some arguments to be passed to nixpkgs, including some
-# patches and also the haskell.nix functionality itself as an overlay.
-, nixpkgsArgs ? haskellNix.nixpkgsArgs
-
-# import nixpkgs with overlays
-, pkgs ? import nixpkgsSrc nixpkgsArgs
-}: pkgs.haskell-nix.project {
-  # 'cleanGit' cleans a source directory based on the files known by git
-  src = pkgs.haskell-nix.haskellLib.cleanGit {
-    name = "haskell-nix-project";
-    src = ./.;
-  };
-  # For `cabal.project` based projects specify the GHC version to use.
-  # compiler-nix-name = "ghc884"; # Not used for `stack.yaml` based projects.
-}
+let
+  pkgsNix = import ./nix/pkgs.nix;
+in
+  # (1)
+  { pkgs ? pkgsNix.native }:
+    # (2)
+    pkgs.haskell-nix.project {
+      src = pkgs.haskell-nix.haskellLib.cleanGit {
+        name = "fission-cli";
+        src = ./. ;
+      };
+    }
