42f002bda24459a4558ec97b9d84dc800e419e07
James Walker <walkah@walkah.net>
2020-12-09 22:51:30 +0000

    store / check last release update timestamp

--- fission-cli/library/Fission/CLI/Release.hs
+++ fission-cli/library/Fission/CLI/Release.hs
@@ -1,12 +1,18 @@
 module Fission.CLI.Release (checkLatestRelease) where
 
 import           Data.Versions
+import qualified Data.Yaml                               as YAML
 import qualified System.Console.ANSI                    as ANSI
 
 import           Fission.Prelude
 
+import           Fission.Error.NotFound.Types
+
+import           Fission.CLI.Environment                as Env
 import qualified Fission.CLI.Meta                       as Meta
 import           Fission.CLI.Display.Text
+import qualified Fission.CLI.Display.Error       as CLI.Error
+
 import qualified Fission.Internal.UTF8                  as UTF8
 
 import           GitHub (github')
@@ -14,27 +20,36 @@ import qualified GitHub
 import           GitHub.Data.Releases                   as Releases
 
 checkLatestRelease ::
-  ( MonadLogger  m
-  , MonadIO      m
-  , MonadCleanup m
+  ( MonadLogger      m
+  , MonadIO          m
+  , MonadTime        m
+  , MonadCleanup     m
+  , MonadEnvironment m
+  , m `Raises` YAML.ParseException
+  , m `Raises` NotFound FilePath
+  , Display (OpenUnion (Errors m))
   )
   => m ()
 checkLatestRelease = do
-  -- check "last checked" timestamp to see if we need to check again
-  logDebug @Text "Checking for newer versions..."
-
-  possibleVersion <- liftIO $ github' GitHub.latestReleaseR "fission-suite" "fission"
-  case possibleVersion of
-    Left  _ -> return () -- just ignore errors here.
-    Right latestRelease -> do
-      let
-        currentVersion = getVersion $ Meta.version =<< Meta.package
-        latestVersion  = getVersion $ Just (Releases.releaseTagName latestRelease)
-      if currentVersion < latestVersion
-        then colourized [ANSI.SetColor ANSI.Foreground ANSI.Vivid ANSI.Yellow] do
-          UTF8.putText $ "⚠️  A new version of Fission CLI is available: "
-          UTF8.putText $ Releases.releaseTagName latestRelease <> " (aka '" <> Releases.releaseName latestRelease <> "')\n"
-        else return () -- no new version
+  attempt Env.get >>= \case
+    Left  err -> CLI.Error.put err "Unable to parse config"
+    Right Env {updateChecked} -> do
+      now            <- currentTime
+      nextUpdateTime <- addUTCTime 86400 updateChecked
+      unless (now < nextUpdateTime) do
+        logDebug @Text "Checking for newer versions..."
+        possibleVersion <- liftIO $ github' GitHub.latestReleaseR "fission-suite" "fission"
+        case possibleVersion of
+          Left  _ -> return () -- just ignore errors here.
+          Right latestRelease -> do
+            let
+              currentVersion = getVersion $ Meta.version =<< Meta.package
+              latestVersion  = getVersion $ Just (Releases.releaseTagName latestRelease)
+            if currentVersion < latestVersion
+              then colourized [ANSI.SetColor ANSI.Foreground ANSI.Vivid ANSI.Yellow] do
+                UTF8.putText $ "⚠️  A new version of Fission CLI is available: "
+                UTF8.putText $ Releases.releaseTagName latestRelease <> " (aka '" <> Releases.releaseName latestRelease <> "')\n"
+              else return () -- no new version
 
 getVersion :: Maybe Text -> Maybe Versioning
 getVersion maybeVersion =
@@ -43,4 +58,4 @@ getVersion maybeVersion =
     Just v  ->
       case versioning v of
         Left _ -> Nothing
-        Right version -> return version
+        Right v' -> return v'
