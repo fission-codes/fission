7b558a7063fa51cad7c6fbedd6267693fd3d7d9a
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-01-24 14:55:26 -0800

    Bump package versions

--- fission-web-server/library/Fission/Web/Server/Meta.hs
+++ fission-web-server/library/Fission/Web/Server/Meta.hs
@@ -1,3 +1,20 @@
-module Fission.Web.Server.Meta (module Fission.Internal.Meta.Package) where
+module Fission.Web.Server.Meta
+  ( package
+  , module Fission.Internal.Meta.Package
+  ) where
 
-import           Fission.Internal.Meta.Package
+import           Data.FileEmbed
+import           Data.Yaml                           as Yaml
+
+import           Fission.Internal.Meta.Package.Types
+
+import           Fission.Prelude
+
+package :: Maybe Package
+package = fromContents $(embedFile "./package.yaml")
+
+fromContents :: ByteString -> Maybe Package
+fromContents contents =
+  case Yaml.decodeEither' contents of
+    Left  _err -> Nothing
+    Right val  -> Just val
