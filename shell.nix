1d1bad78ef93152e7fcd67ab4dd9f754eeda6e3b
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-01-26 23:32:03 +0000

    Fix Nix

--- shell.nix
+++ shell.nix
@@ -6,16 +6,27 @@ let
 in
 
 pkgs.mkShell {
+  nativeBuildInputs = [pkgs.pkg-config];
+
   buildInputs = [
+    pkgs.figlet
+    pkgs.lolcat
+
     pkgs.curl
     pkgs.gnumake
     unstable.niv
-    pkgs.zlib
+
+    pkgs.lzma.dev   
+    pkgs.lzma.out
+
+    # pkgs.zlib.dev
+    # pkgs.zlib.out
 
     # Data
     pkgs.ipfs
     pkgs.haskellPackages.postgresql-libpq
-    pkgs.openssl
+    pkgs.openssl.dev
+    pkgs.openssl.out
     pkgs.postgresql
 
     hinotify
@@ -28,4 +39,11 @@ pkgs.mkShell {
     unstable.haskell-language-server
     unstable.haskellPackages.implicit-hie
   ];
+
+  shellHook = ''
+    export LANG=C.UTF8
+
+    echo "Welcome to the"
+    ${pkgs.figlet}/bin/figlet "Fission Project Shell" | lolcat -a -s 50
+  '';
 }
