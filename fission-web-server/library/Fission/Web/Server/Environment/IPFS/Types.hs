fe48c530d8a63456fa4d2dffe1c314513cdf261c
Daniel Holmgren <dtholmgren@gmail.com>
2021-01-28 14:24:52 -0600

    allow multiple remote peers (#445)

--- fission-web-server/library/Fission/Web/Server/Environment/IPFS/Types.hs
+++ fission-web-server/library/Fission/Web/Server/Environment/IPFS/Types.hs
@@ -6,19 +6,19 @@ import qualified Network.IPFS.Types as IPFS
 import           Fission.Prelude
 
 data Environment = Environment
-  { url        :: IPFS.URL     -- ^ IPFS client URL (may be remote)
-  , timeout    :: IPFS.Timeout -- ^ IPFS timeout in seconds
-  , binPath    :: IPFS.BinPath -- ^ Path to local IPFS binary
-  , gateway    :: IPFS.Gateway -- ^ Domain Name of IPFS Gateway
-  , remotePeer :: IPFS.Peer    -- ^ Remote Peer to connect to
+  { url         :: IPFS.URL           -- ^ IPFS client URL (may be remote)
+  , timeout     :: IPFS.Timeout       -- ^ IPFS timeout in seconds
+  , binPath     :: IPFS.BinPath       -- ^ Path to local IPFS binary
+  , gateway     :: IPFS.Gateway       -- ^ Domain Name of IPFS Gateway
+  , remotePeers :: NonEmpty IPFS.Peer -- ^ Remote Peer to connect to
   } deriving Show
 
 instance FromJSON Environment where
   parseJSON = withObject "IPFS.Environment" \obj -> do
-    timeout    <- obj .:? "timeout" .!= 3600
-    binPath    <- obj .:? "binPath" .!= "/usr/local/bin/ipfs"
-    gateway    <- obj .:? "gateway" .!= "ipfs.runfission.com"
-    url        <- obj .:  "url" >>= parseJSON . String
-    remotePeer <- obj .:  "remotePeer"
+    timeout     <- obj .:? "timeout" .!= 3600
+    binPath     <- obj .:? "binPath" .!= "/usr/local/bin/ipfs"
+    gateway     <- obj .:? "gateway" .!= "ipfs.runfission.com"
+    url         <- obj .:  "url" >>= parseJSON . String
+    remotePeers <- obj .:  "remotePeers"
 
     return Environment {..}
