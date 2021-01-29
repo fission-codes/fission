0a28ad13844d6d26be0997dc6485dc196f44466d
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-01-28 22:32:08 -0800

    Working on mac in pure mode(!)

--- shell.nix
+++ shell.nix
@@ -1,64 +1,71 @@
 let
   sources  = import ./nix/sources.nix;
-  pkgs     = import sources.nixpkgs  {};
+
+  nixos    = import sources.nixpkgs  {};
+  darwin   = import sources.darwin   {};
   unstable = import sources.unstable {};
-  hinotify = if pkgs.stdenv.isDarwin then pkgs.haskellPackages.hfsevents else pkgs.haskellPackages.hinotify;
+
+  pkgs     = if darwin.stdenv.isDarwin then darwin else nixos;
   ghc      = unstable.ghc;
+
+  deps = {
+    common = [ 
+      pkgs.gnumake
+      unstable.niv
+    ];
+
+    crypto = [ 
+      pkgs.openssl.dev
+      pkgs.openssl.out
+    ];
+
+    cli = [pkgs.ncurses.dev.out];
+
+    data = [
+      pkgs.ipfs
+      pkgs.haskellPackages.postgresql-libpq
+      pkgs.lzma.dev   
+      pkgs.lzma.out
+      pkgs.zlib.dev
+      pkgs.zlib.out
+      pkgs.postgresql
+    ];
+
+    haskell = [
+      # unstable.ghcid
+      unstable.ghc
+      unstable.stack
+      # unstable.stylish-haskell
+      # unstable.haskellPackages.hie-bios
+      # unstable.haskell-language-server
+      # unstable.haskellPackages.implicit-hie
+    ];
+
+    fun = [
+      pkgs.figlet
+      pkgs.lolcat
+    ];
+   
+    macos =
+      if pkgs.stdenv.isDarwin then
+        [ unstable.darwin.apple_sdk.frameworks.CoreServices
+          unstable.darwin.apple_sdk.frameworks.Foundation
+          unstable.darwin.apple_sdk.frameworks.Cocoa
+        ]
+      else 
+        [];
+  };
 in
 
-# unstable.mkShell {
 unstable.haskell.lib.buildStackProject {
   inherit ghc;
   name = "Fisson";
-  nativeBuildInputs = [
-    # Basics
-    pkgs.gnumake
-    unstable.niv
-
-    # Crypto
-    # pkgs.openssl
-    pkgs.openssl.dev
-    pkgs.openssl.out
-
-    # CLI
-    pkgs.ncurses.dev.out
-
-    # Data
-    pkgs.ipfs
-    pkgs.haskellPackages.postgresql-libpq
-    pkgs.lzma.dev   
-    pkgs.lzma.out
-    pkgs.zlib.dev
-    pkgs.zlib.out
-    pkgs.postgresql
-
-    hinotify
-
-    # Haskell Tooling
-    # unstable.ghcid
-    unstable.ghc
-    unstable.stack
-    # unstable.stylish-haskell
-    # unstable.haskellPackages.hie-bios
-    # unstable.haskell-language-server
-    # unstable.haskellPackages.implicit-hie
-
-
-    unstable.darwin.apple_sdk.frameworks.CoreServices
-    unstable.darwin.apple_sdk.frameworks.Foundation
-    unstable.darwin.apple_sdk.frameworks.Cocoa
-
-
-
-    # Fun
-    pkgs.figlet
-    pkgs.lolcat
-  ];
+  nativeBuildInputs = deps.common ++ deps.crypto ++ deps.cli ++ deps.data ++ deps.macos ++ deps.haskell ++ deps.fun;
 
   shellHook = ''
     export LANG=C.UTF8
   
-    echo "Welcome to the"
-    ${pkgs.figlet}/bin/figlet "Fission Shell" | lolcat -a -s 50
+    echo "Welcome to the..."
+    ${pkgs.figlet}/bin/figlet "Glorious Fission Dev Environment" | lolcat -a -s 50
   '';
 }
