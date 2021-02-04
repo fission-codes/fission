7624630180c60216d85fca2c52c05b66cf4e8319
Daniel Holmgren <dtholmgren@gmail.com>
2021-02-04 13:52:04 -0700

    update comment

--- fission-web-server/library/Fission/Web/Server/IPFS/Cluster/Class.hs
+++ fission-web-server/library/Fission/Web/Server/IPFS/Cluster/Class.hs
@@ -6,6 +6,6 @@ import qualified Network.IPFS.Types                          as IPFS
 
 import qualified Fission.Web.Server.IPFS.Cluster.Error.Types as Cluster
 
--- | A monad representing the ability to pin a CID, either to an IPFS node or a cluster
+-- | A monad giving access an IPFS Cluster Daemon
 class Monad m => MonadIPFSCluster m where
   pin :: IPFS.CID -> m (Either Cluster.Error ())
