14411b96c9f01e3552168792c2c3c911c85c669b
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-02-16 15:17:47 -0800

    Add a couple quick tests

--- fission-web-server/test/Main.hs
+++ fission-web-server/test/Main.hs
@@ -3,13 +3,15 @@ module Main (main) where
 import           Test.Fission.Prelude
 import qualified Test.Fission.Random
 
-import qualified Test.Fission.DNS      as DNS
-import qualified Test.Fission.Error    as Error
+import qualified Test.Fission.DNS                                       as DNS
+import qualified Test.Fission.Error                                     as Error
 
-import qualified Test.Fission.User.DID as DID
+import qualified Test.Fission.User.DID                                  as DID
 
-import qualified Test.Fission.Web.Auth as Web.Auth
-import qualified Test.Fission.Web.Ping as Web.Ping
+import qualified Test.Fission.Web.Auth                                  as Web.Auth
+import qualified Test.Fission.Web.Ping                                  as Web.Ping
+
+import qualified Test.Fission.Web.Server.IPFS.Cluster.Pin.Global.Status as Cluster
 
 main :: IO ()
 main = defaultMain =<< tests
@@ -22,5 +24,6 @@ tests =
     , Error.tests
     , DID.tests
     , DNS.tests
+    , Cluster.tests
     , Test.Fission.Random.tests
     ]
