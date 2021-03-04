c72dce9ac4ff00398c93d7375ca71ae1d0871b88
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-03-04 12:28:10 -0800

    Update with more/better server commands

--- nix/commands.nix
+++ nix/commands.nix
@@ -1,9 +1,11 @@
 { pkgs, unstable, ... }:
   let
     bash  = "${pkgs.bash}/bin/bash";
+    ghcid = "${pkgs.ghcid}/bin/ghcid";
+    git   = "${pkgs.git}/bin/git";
+    lsof  = "${pkgs.lsof}/bin/lsof";
     ssh   = "${pkgs.openssh}/bin/ssh";
     stack = "${unstable.stack}/bin/stack";
-    ghcid = "${pkgs.ghcid}/bin/ghcid";
 
     cmd = description: script:
       { inherit description;
@@ -82,18 +84,38 @@
       in
         [helper] ++ packages;
 
+    server-path = ~/.local/bin/server;
+    server-port = 10235;
+
+    # This will be much better when we have a nix-build
+    server-install = cmd "Install the Fission Server" "${stack} install fission-web-server:server";
+    server-start   = cmd "Run the currently installed Fission Server"   "DEBUG=true nohup ${server-path} &";
+    server-debug   = cmd "Run the Fission Server in debug verbose mode" "DEBUG=true ${server-path}";
+
   in
     commands {
-      build          = cmd "Build entire project"       "${stack} build";
-      install-cli    = cmd "Install the Fission CLI"    "${stack} install fission-cli:fission";
-      install-server = cmd "Install the Fission Server" "${stack} install fission-web-server:server";
+      build       = cmd "Build entire project"    "${stack} build";
+      cli-install = cmd "Install the Fission CLI" "${stack} install fission-cli:fission";
+
+      inherit
+        server-install
+        server-start
+        server-debug;
+
+      server-update = cmd "Update & run the current server to the latest on the current branch" ''
+        ${git} pull
+        ${server-install.script}
+        kill $(sudo ${lsof} -t -i :${toString server-port})
+        printf "\033[31m\n\n\n>>>>> NOTE: Don't forget to release a new version of the CLI<<<<<\033[0m\n"
+        ${server-start.script}
+      '';
 
       runtests = cmd "Run the complete test suite" "${stack} test";
       repl     = cmd "Enter the project REPL"      "${stack} repl  --no-nix-pure";
       watch    = cmd "Autobuild with file watcher" "${stack} build --file-watch";
 
       ssh-staging = cmd "SSH into the staging environment"
-        "${ssh} ubuntu@instance.runfission.net";
+        "${ssh} -v fission@instance.runfission.net";
 
       ssh-prod = cmd "SSH into the production environment"
         "${ssh} ubuntu@instance.runfission.com";
