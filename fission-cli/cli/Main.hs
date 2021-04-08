21b6a82aca9f72d52d091966e484cd7a76de937d
Philipp Krüger <philipp.krueger1@gmail.com>
2021-04-08 18:59:17 +0200

    Enforce utf-8 encoded output

--- fission-cli/cli/Main.hs
+++ fission-cli/cli/Main.hs
@@ -2,10 +2,13 @@ module Main (main) where
 
 import           Fission.Prelude
 
+import           GHC.IO.Encoding as Encoding
+
 import           Fission.CLI
 
 main :: IO ()
-main =
+main = do
+  Encoding.setLocaleEncoding Encoding.utf8
   cli >>= \case
     Left  _ -> exitFailure
     Right _ -> exitSuccess
