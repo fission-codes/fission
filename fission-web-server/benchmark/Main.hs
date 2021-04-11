c3a0a2c6620a6ad7772a5dd4cf09ca65ab81147d
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-04-11 16:01:51 -0700

    Add RSA-2048 / UCAN benchmarks

--- fission-web-server/benchmark/Main.hs
+++ fission-web-server/benchmark/Main.hs
@@ -1,19 +1,20 @@
 module Main (main) where
 
-import Criterion
-import Criterion.Main
-import RIO
-
-import qualified Fission.Internal.UTF8 as UTF8
+import           RIO
 import qualified RIO.ByteString.Lazy   as Lazy
 
+import qualified Fission.Internal.UTF8 as UTF8
+
+import           Test.Tasty.Bench
+
 main :: IO ()
-main = defaultMain
-  [ bgroup "Textable.encode" encode
-  ]
+main =
+  defaultMain
+    [ bgroup "Textable.encode" encode
+    ]
 
 encode :: [Benchmark]
 encode =
-  [ bench "Strict ByteString" $ whnf UTF8.encode ("hello world" :: ByteString)
-  , bench "Lazy ByteString" $ whnf UTF8.encode ("hello world" :: Lazy.ByteString)
+  [ bench "Strict ByteString" $ nf UTF8.encode ("hello world" :: ByteString)
+  , bench "Lazy ByteString"   $ nf UTF8.encode ("hello world" :: Lazy.ByteString)
   ]
