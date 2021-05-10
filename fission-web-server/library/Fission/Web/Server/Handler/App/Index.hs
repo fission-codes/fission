c31324a2a86a33ed7065dc47b18d122875b32cf1
Brooklyn Zelenka <be.zelenka@gmail.com>
2021-05-09 21:59:08 -0700

    Bump versions, module consistency, remove unused comment (#510)

--- fission-web-server/library/Fission/Web/Server/Handler/App/Index.hs
+++ fission-web-server/library/Fission/Web/Server/Handler/App/Index.hs
@@ -6,7 +6,6 @@ import           Servant
 
 import           Fission.Prelude
 
-import           Fission.App.Types
 import           Fission.URL.Types
 
 import           Fission.Web.API.App.Index.Types
@@ -21,13 +20,26 @@ index :: (MonadDB t m, App.Retriever t, App.Domain.Retriever t) => ServerT Index
 index Authorization {about = Entity userId _} = runDB do
   apps        <- App.ownedBy userId
   appXDomains <- forM apps findDomains
-  let normalized = (\(k, v) -> (fromIntegral (fromSqlKey k), v)) <$> appXDomains
-  return (Map.fromList normalized)
+  return $ Map.fromList appXDomains
+
   where
-    findDomains :: App.Domain.Retriever m => Entity App -> m (AppId, Payload)
+    findDomains :: App.Domain.Retriever m => Entity App -> m (Natural, Payload)
     findDomains (Entity appId App {appInsertedAt, appModifiedAt}) = do
       appDomains <- App.Domain.allForApp appId
-      return (appId, Payload (toURL <$> appDomains) appInsertedAt appModifiedAt)
+
+      let
+        natKey :: Natural
+        natKey = fromIntegral $ fromSqlKey appId
+
+        payload :: Payload
+        payload =
+          Payload
+            { urls       = toURL <$> appDomains
+            , insertedAt = appInsertedAt
+            , modifiedAt = appModifiedAt
+            }
+
+      return (natKey, payload)
 
     toURL :: Entity AppDomain -> URL
     toURL (Entity _ AppDomain {..}) = URL appDomainDomainName appDomainSubdomain
