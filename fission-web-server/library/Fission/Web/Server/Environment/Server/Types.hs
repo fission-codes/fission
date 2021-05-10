4a7845b70b23167d5a7250b5f6dfb238029b6f09
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-05-09 22:45:31 -0700

    Add wrk2, default EKG to false

--- fission-web-server/library/Fission/Web/Server/Environment/Server/Types.hs
+++ fission-web-server/library/Fission/Web/Server/Environment/Server/Types.hs
@@ -26,7 +26,7 @@ instance FromJSON Environment where
     sentryDSN    <- obj .:? "sentry_dsn"
     pretty       <- obj .:? "pretty" .!= False
     isTLS        <- obj .:? "tls"    .!= True
-    useEKG       <- obj .:? "ekg"    .!= True
+    useEKG       <- obj .:? "ekg"    .!= False
     port         <- obj .:? "port"   .!= Web.Port if isTLS then 443 else 80
     host         <- obj .:  "host"
     environment  <- obj .:  "environment"
