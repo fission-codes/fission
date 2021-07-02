d9a183973a39e0638b3ea04a0d8e9fb7345ef1c7
Brooklyn Zelenka <hello@brooklynzelenka.com>
2021-07-02 15:55:05 -0700

    Purge on app destroy

--- fission-web-server/library/Fission/Web/Server/Internal/Orphanage/ServerError.hs
+++ fission-web-server/library/Fission/Web/Server/Internal/Orphanage/ServerError.hs
@@ -3,7 +3,12 @@
 module Fission.Web.Server.Internal.Orphanage.ServerError () where
 
 import           RIO
+import qualified RIO.Text       as Text
+
 import           Servant.Server
 
 instance Display ServerError where
   display = displayShow
+
+instance Display [ServerError] where
+  textDisplay errs = Text.intercalate ", " $ fmap textDisplay errs
--- fission-web-server/library/Fission/Web/Server/Types.hs
+++ fission-web-server/library/Fission/Web/Server/Types.hs
@@ -4,100 +4,107 @@ module Fission.Web.Server.Types
   , module Fission.Web.Server.Config.Types
   ) where
 
-import           Control.Monad.Catch                       hiding (finally)
+import           Control.Monad.Catch                               hiding
+                                                                   (finally)
 import           Control.Monad.Except
 
-import qualified RIO.ByteString.Lazy                       as Lazy
-import           RIO.NonEmpty                              as NonEmpty
-import           RIO.NonEmpty.Partial                      as NonEmpty.Partial
-import qualified RIO.Text                                  as Text
+import qualified RIO.ByteString.Lazy                               as Lazy
+import           RIO.NonEmpty                                      as NonEmpty
+import           RIO.NonEmpty.Partial                              as NonEmpty.Partial
+import qualified RIO.Text                                          as Text
 
-import           System.Random                             as Random
+import           System.Random                                     as Random
 
-import           Database.Esqueleto                        as SQL hiding ((<&>))
+import           Database.Esqueleto                                as SQL hiding
+                                                                          ((<&>))
 
 import           Servant.Client
-import qualified Servant.Client.Streaming                  as Stream
+import qualified Servant.Client.Streaming                          as Stream
 import           Servant.Server.Experimental.Auth
-import qualified Servant.Types.SourceT                     as Stream
+import qualified Servant.Types.SourceT                             as Stream
 
-import           Network.AWS                               as AWS hiding
-                                                                  (Request,
-                                                                   Seconds)
+import qualified Network.HTTP.Req                                  as HTTP
+
+import           Network.AWS                                       as AWS hiding
+                                                                          (Request,
+                                                                           Seconds)
 import           Network.AWS.Route53
 
-import qualified Network.IPFS                              as IPFS
-import qualified Network.IPFS.Add.Error                    as IPFS.Pin
+import qualified Network.IPFS                                      as IPFS
+import qualified Network.IPFS.Add.Error                            as IPFS.Pin
 import           Network.IPFS.Client.Streaming.Pin
 import           Network.IPFS.File.Types
-import qualified Network.IPFS.Stat                         as IPFS.Stat
-import qualified Network.IPFS.Types                        as IPFS
+import qualified Network.IPFS.Stat                                 as IPFS.Stat
+import qualified Network.IPFS.Types                                as IPFS
 
 import           Fission.Prelude
 
-import qualified Fission.Internal.UTF8                     as UTF8
+import qualified Fission.Internal.UTF8                             as UTF8
 
-import           Fission.Error                             as Error
+import           Fission.Error                                     as Error
 import           Fission.Error.GenericError.Types
+import qualified Fission.JSON.Error                                as JSON
 import           Fission.Time
 
-import           Fission.Web.Server.AWS.Types              as AWS
+import           Fission.Web.Server.AWS.Types                      as AWS
 import           Fission.Web.Server.Models
 
-import qualified Fission.Web.Server.DID.Publicize.Class    as Server.DID
+import qualified Fission.Web.Server.DID.Publicize.Class            as Server.DID
+import           Fission.Web.Server.Host.Types
 
-import           Fission.DNS                               as DNS
-import           Fission.URL                               as URL
+import           Fission.DNS                                       as DNS
+import           Fission.URL                                       as URL
 
 import           Fission.Web.API.Host.Types
 import           Fission.Web.Async
 
-import qualified Fission.Web.Server.App                    as App
-import qualified Fission.Web.Server.App.Destroyer          as App.Destroyer
-import qualified Fission.Web.Server.Error                  as Web.Error
-import           Fission.Web.Server.WNFS                   as WNFS
+import qualified Fission.Web.Server.App                            as App
+import qualified Fission.Web.Server.App.Destroyer                  as App.Destroyer
+import qualified Fission.Web.Server.Error                          as Web.Error
+import           Fission.Web.Server.WNFS                           as WNFS
 
-import           Fission.Web.Server.IPFS.Cluster           as Cluster
-import           Fission.Web.Server.IPFS.DNSLink           as DNSLink
+import           Fission.Web.Server.IPFS.Cluster                   as Cluster
+import           Fission.Web.Server.IPFS.DNSLink                   as DNSLink
 import           Fission.Web.Server.IPFS.Linked
 
-import qualified Fission.Web.Server.Heroku.AddOn.Creator   as Heroku.AddOn
-import           Fission.Web.Server.Heroku.Types           as Heroku
+import qualified Fission.Web.Server.Heroku.AddOn.Creator           as Heroku.AddOn
+import           Fission.Web.Server.Heroku.Types                   as Heroku
 
-import           Fission.Web.Server.AWS                    as AWS
-import           Fission.Web.Server.AWS.Route53            as Route53
+import           Fission.Web.Server.AWS                            as AWS
+import           Fission.Web.Server.AWS.Route53                    as Route53
 import           Fission.Web.Server.Authorization.Types
 
-import           Fission.Web.Server.Auth                   as Auth
-import qualified Fission.Web.Server.Auth.DID               as Auth.DID
-import qualified Fission.Web.Server.Auth.Token             as Auth.Token
+import           Fission.Web.Server.Auth                           as Auth
+import qualified Fission.Web.Server.Auth.DID                       as Auth.DID
+import qualified Fission.Web.Server.Auth.Token                     as Auth.Token
 
 import           Fission.Web.Server.Handler
-import           Fission.Web.Server.Reflective             as Reflective
+import           Fission.Web.Server.Reflective                     as Reflective
 
-import           Fission.User.DID                          as DID
-import qualified Fission.User.DID.Oldstyle.Types           as DID
+import           Fission.User.DID                                  as DID
+import qualified Fission.User.DID.Oldstyle.Types                   as DID
 
-import qualified Fission.Web.Server.User                   as User
+import qualified Fission.Web.Server.User                           as User
 import           Fission.Web.Server.User.Creator.Class
-import qualified Fission.Web.Server.User.Modifier.Class    as User.Modifier
-import qualified Fission.Web.Server.User.Password          as Password
+import qualified Fission.Web.Server.User.Modifier.Class            as User.Modifier
+import qualified Fission.Web.Server.User.Password                  as Password
 
-import qualified Fission.Key                               as Key
+import qualified Fission.Key                                       as Key
 
 import           Fission.Web.Server.MonadDB
 
-import qualified Fission.Web.Auth.Token.JWT.RawContent     as JWT
-import           Fission.Web.Auth.Token.JWT.Resolver       as JWT
+import qualified Fission.Web.Auth.Token.JWT.RawContent             as JWT
+import           Fission.Web.Auth.Token.JWT.Resolver               as JWT
 
 import           Fission.Authorization.ServerDID.Class
+import qualified Fission.Web.Server.Internal.NGINX.Purge           as NGINX
 
-import           Fission.Web.Server.App.Content            as App.Content
-import           Fission.Web.Server.App.Domain             as App.Domain
+import           Fission.Web.Server.App.Content                    as App.Content
+import           Fission.Web.Server.App.Domain                     as App.Domain
 
-import           Fission.Web.Server.Challenge              as Challenge
-import qualified Fission.Web.Server.Domain                 as Domain
-import qualified Fission.Web.Server.Email                  as Email
+import           Fission.Web.Server.Challenge                      as Challenge
+import qualified Fission.Web.Server.Domain                         as Domain
+import qualified Fission.Web.Server.Email                          as Email
 import           Fission.Web.Server.Email.Class
 import           Fission.Web.Server.RecoveryChallenge      as RecoveryChallenge
 
@@ -105,6 +112,8 @@ import           Fission.Web.Server.Auth.Token.Basic.Class
 import           Fission.Web.Server.Relay.Store.Class
 
 import           Fission.Web.Server.Config.Types
+
+import           Fission.Web.Server.Internal.Orphanage.ServerError ()
 
 -- | The top-level app type
 newtype Server a = Server { unServer :: RIO Config a }
@@ -136,6 +145,12 @@ instance MonadDB (Transaction Server) Server where
   runDB transaction = do
     pool <- asks dbPool
     SQL.runSqlPool transaction pool
+
+instance HTTP.MonadHttp Server where
+  -- TODO switch to MonadRaise.raise when that's available in this package
+  handleHttpException = \case
+    HTTP.VanillaHttpException httpException -> throwM httpException
+    HTTP.JsonHttpException txt              ->  throwM $ JSON.Error txt
 
 instance MonadRelayStore Server where
   getStoreVar = asks linkRelayStoreVar
@@ -680,8 +695,23 @@ instance App.Modifier Server where
 instance App.Destroyer Server where
   destroy uId appId now =
     runDB (App.destroy uId appId now) >>= \case
-      Left err   -> return $ Left err
-      Right urls -> pullFromDNS urls
+      Left err   ->
+        return $ Left err
+
+      Right urls ->
+        pullFromDNS urls >>= \case
+          Left errs ->
+            return $ Left errs
+
+          Right _ -> do
+            NGINX.purgeMany urls >>= \case
+              Left errs -> do
+                -- Just warn and continue
+                logWarn $ textDisplay errs
+                return $ Right urls
+
+              Right _ ->
+                return $ Right urls
 
   destroyByURL uId domainName maySubdomain now =
     runDB (App.destroyByURL uId domainName maySubdomain now) >>= \case
