34a09c174ab6d618c6a9217c2f0d84f74ab19c7f
Brooklyn Zelenka <brooklyn@fission.codes>
2021-09-10 03:26:22 +0000

    Add docs summary

--- fission-web-api/library/Fission/Web/API/Docs/Types.hs
+++ fission-web-api/library/Fission/Web/API/Docs/Types.hs
@@ -2,4 +2,7 @@ module Fission.Web.API.Docs.Types (Docs) where
 
 import           Servant.Swagger.UI.Core
 
-type Docs = SwaggerSchemaUI "docs" "docs.json"
+type Docs
+  =  Summary "Docs"
+  :> Description "Swagger-based documentation"
+  :> SwaggerSchemaUI "docs" "docs.json"
