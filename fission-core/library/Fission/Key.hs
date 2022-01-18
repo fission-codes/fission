c2b628a6de25a2541224d54bce2367b1edc19924
Philipp Krüger <philipp.krueger1@gmail.com>
2022-01-18 16:37:30 +0100

    Also re-export symmetric in Fission/Key

--- fission-core/library/Fission/Key.hs
+++ fission-core/library/Fission/Key.hs
@@ -4,7 +4,9 @@ module Fission.Key
   ( -- * Reexports
     module Crypto.Key.Asymmetric
   , module Fission.Key.Error
+  , module Fission.Key.Symmetric
   ) where
 
 import           Crypto.Key.Asymmetric
 import           Fission.Key.Error
+import           Fission.Key.Symmetric
