b6504d91783f77bcb9cc3e32cc1dca56561db76c
Philipp Krüger <philipp.krueger1@gmail.com>
2022-01-19 15:40:54 +0100

    Format files

--- hs-ucan/library/Web/Ucan/Resolver/Class.hs
+++ hs-ucan/library/Web/Ucan/Resolver/Class.hs
@@ -4,8 +4,8 @@ import           Network.IPFS.CID.Types
 
 import           RIO
 
-import           Web.Ucan.Resolver.Error as Resolver
 import qualified Web.Ucan.RawContent     as Ucan
+import           Web.Ucan.Resolver.Error as Resolver
 
 class Monad m => Resolver m where
   resolve :: CID -> m (Either Resolver.Error Ucan.RawContent)
