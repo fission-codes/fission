729b58f5ce715a2f5cbee477fd3b45846f041866
Philipp Krüger <philipp.krueger1@gmail.com>
2022-02-18 17:10:46 +0100

    Short-circuit on invalid witnesses

--- hs-ucan/library/Web/UCAN/Capabilities/Error.hs
+++ hs-ucan/library/Web/UCAN/Capabilities/Error.hs
@@ -3,10 +3,15 @@ module Web.UCAN.Capabilities.Error
   ) where
 
 import           RIO
+import           RIO.Time
+import           Web.DID.Types
 import qualified Web.UCAN.Resolver.Error as Resolver
 
 
 data Error
   = ResolveError Resolver.Error
   | ParseError String
+  | DelegationIssuerAudienceMismatch DID DID
+  | DelegationNotBeforeWitnessExpired UTCTime UTCTime
+  | DelegationExpiresAfterNotBefore UTCTime UTCTime
   deriving (Show, Eq, Exception)
