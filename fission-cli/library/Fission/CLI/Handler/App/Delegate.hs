d26319821ace2473fd2ca4092be078c757c5a3d8
Brian Ginsburg <gins@brianginsburg.com>
2022-06-09 14:12:54 -0700

    Add app registration check

--- fission-cli/library/Fission/CLI/Handler/App/Delegate.hs
+++ fission-cli/library/Fission/CLI/Handler/App/Delegate.hs
@@ -2,6 +2,10 @@
 module Fission.CLI.Handler.App.Delegate (delegate) where
 
 import qualified Crypto.PubKey.Ed25519                     as Ed25519
+import           Data.Function
+import qualified Data.Yaml                                 as YAML
+import qualified RIO.Text                                  as Text
+import qualified RIO.Map                                   as Map
 import qualified System.Console.ANSI                       as ANSI
 
 import           Fission.Prelude
@@ -12,6 +16,7 @@ import           Fission.Error.Types
 import qualified Fission.Internal.UTF8                     as UTF8
 
 import           Fission.Web.Auth.Token.Types
+import           Fission.Web.Auth.Token.UCAN.Types
 import           Fission.Web.Client
 
 import           Fission.CLI.Display.Text
@@ -27,26 +32,30 @@ import           Fission.CLI.WebNative.Mutation.Auth.Store as UCAN
 
 import           Web.DID.Types
 
+import Fission.Web.API.App.Index.Payload.Types
+
 -- | Delegate capabilities to a key pair or DID
 delegate ::
-  ( MonadLogger m 
-  -- , MonadTime        m
+  ( MonadIO          m
+  , MonadLogger      m 
+  , MonadTime        m
 
-  -- , MonadEnvironment m
-  -- , UCAN.MonadStore  m
-  -- , MonadWebClient   m
-  -- , ServerDID        m
+  , MonadEnvironment m
+  , UCAN.MonadStore  m
+  , MonadWebClient   m
+  , ServerDID        m
 
-  -- , MonadCleanup     m
-  -- , m `Raises` ClientError
-  -- , m `Raises` NotFound FilePath
+  , MonadCleanup     m
+  , m `Raises` ClientError
+  , m `Raises` YAML.ParseException
+  , m `Raises` NotFound FilePath
 
-  -- , Contains (Errors m) (Errors m)
-  -- , Display  (OpenUnion (Errors m))
-  -- , Show     (OpenUnion (Errors m))
+  , Contains (Errors m) (Errors m)
+  , Display  (OpenUnion (Errors m))
+  , Show     (OpenUnion (Errors m))
 
-  -- , MonadWebAuth m Token
-  -- , MonadWebAuth m Ed25519.SecretKey
+  , MonadWebAuth m Token
+  , MonadWebAuth m Ed25519.SecretKey
   )
   => Text
   -> Bool 
@@ -54,3 +63,50 @@ delegate ::
   -> m ()
 delegate appName generateKey mayAudienceDid = do
     logDebug @Text "delegate"
+
+    proof <- getRootUserProof
+    appRegistered <- checkAppRegistration appName proof
+
+    if appRegistered then
+      logDebug $ appName <> " is registered!"
+    else
+      logDebug $ appName <> " is not registered."
+
+
+checkAppRegistration :: 
+  ( MonadIO          m
+  , MonadLogger      m 
+  , MonadTime        m
+
+  , MonadWebClient   m
+  , ServerDID        m
+
+  , MonadCleanup     m
+  , m `Raises` ClientError
+
+  , Contains (Errors m) (Errors m)
+  , Display  (OpenUnion (Errors m))
+  , Show     (OpenUnion (Errors m))
+
+  , MonadWebAuth m Token
+  , MonadWebAuth m Ed25519.SecretKey
+  )
+  => Text 
+  -> Proof 
+  -> m Bool 
+checkAppRegistration appName proof = do
+    attempt (sendAuthedRequest proof appIndex) >>= \case
+      Left err -> do
+        logDebug $ textDisplay err
+        CLI.Error.put err $ textDisplay err
+        raise err
+
+      Right index -> do
+        let
+          registered = 
+            Map.elems index
+              & concatMap (\Payload { urls } -> urls)
+              & fmap (fst . Text.break (== '.') . textDisplay)
+              & elem appName
+
+        return registered
