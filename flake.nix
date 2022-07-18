43b454c7d31e14154ffad7980b807e1ecead5fe5
Steven Vandevelde <icid.asset@gmail.com>
2022-07-18 17:05:06 +0200

    Fix secp256k1 lib binding

--- flake.nix
+++ flake.nix
@@ -59,6 +59,10 @@
               pkgs.secp256k1
               pkgs.stylish-haskell
               tasks
+
+              # https://nixos.wiki/wiki/FAQ/I_installed_a_library_but_my_compiler_is_not_finding_it._Why%3F
+              # Needed for binding secp256k1
+              pkgs.pkg-config
             ];
             NIX_PATH = "nixpkgs=" + pkgs.path;
           };
