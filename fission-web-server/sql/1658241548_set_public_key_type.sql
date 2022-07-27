929f3f68a0b64a9ec3340731db0795b1e3902660
Steven Vandevelde <icid.asset@gmail.com>
2022-07-27 18:48:09 +0200

    Update SQL migration to new types

--- fission-web-server/sql/1658241548_set_public_key_type.sql
+++ fission-web-server/sql/1658241548_set_public_key_type.sql
@@ -7,10 +7,10 @@ UPDATE "user" SET public_key = (
 
   -- Before this migration was created we only had ED25519 and RSA keys
   WHEN starts_with(public_key, 'MII') THEN
-    format('{"key":"%s","type":"%s"}', public_key, 'RSAPublicKey')
+    format('{"key":"%s","type":"%s"}', public_key, 'RSA')
 
   ELSE
-    format('{"key":"%s","type":"%s"}', public_key, 'Ed25519PublicKey')
+    format('{"key":"%s","type":"%s"}', public_key, 'Ed25519')
 
   END
 )
