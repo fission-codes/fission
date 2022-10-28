c5c49a4c9a94196a7617c684187d99d4d5cc3615
Brooklyn Zelenka <hello@brooklynzelenka.com>
2022-10-28 14:11:29 +0100

    Hacky version of UCAN fact objects

--- fission-core/library/Fission/Web/Auth/Token/UCAN/Fact/Types.hs
+++ fission-core/library/Fission/Web/Auth/Token/UCAN/Fact/Types.hs
@@ -2,6 +2,7 @@ module Fission.Web.Auth.Token.UCAN.Fact.Types (Fact (..)) where
 
 import           Crypto.Cipher.AES           (AES256)
 import qualified RIO.Text                    as Text
+import           Servant.API
 
 import           Fission.Prelude
 
@@ -31,12 +32,26 @@ instance ToJSON Fact where
     Unknown    txt -> String txt
 
 instance FromJSON Fact where
+  parseJSONList = withArray "[]" \vec ->
+    sequence $ fmap parseJSON $ toList vec
+
   parseJSON jsn =
-    parseSessionKey jsn <|> parseUnknown jsn <|> fail "Unable to parse UCAN.Fact"
+    parseSessionKey jsn <|> parseUnknown jsn <|> parseObj jsn <|> fail "Unable to parse UCAN.Fact"
     where
-      parseUnknown = withText "UCAN.Fact.Unknown" \txt -> return $ Unknown txt
+      parseUnknown =
+        withText "UCAN.Fact.Unknown" \txt -> return $ Unknown txt
+
+      parseObj = withObject "UCAN.Fact.UnkownObj" \obj ->
+        return . Unknown . Text.pack $ show obj
 
       parseSessionKey = withObject "UCAN.Fact SessionKey" \obj -> do
         rawKey <- obj .: "sessionKey"
         key    <- parseJSON $ String rawKey
         return $ SessionKey key
+
+
+instance FromHttpApiData Fact where
+  parseUrlPiece txt =
+    case parseUrlPiece txt of
+      Left _    -> pure $ Unknown txt
+      Right key -> pure $ SessionKey key
