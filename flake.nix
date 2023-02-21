71be18ac03da9e2b75ffcd0d6b426958a5048fb4
James Walker <walkah@walkah.net>
2023-02-21 17:11:25 -0500

    clean up, use our own cache, etc

--- flake.nix
+++ flake.nix
@@ -62,29 +62,8 @@
         ];
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
         flake = pkgs.fission.flake { };
-
-        # The default version of HLS (with binary cache) is built with GHC 9.0.1
-        # We can get this version working with our current set up, but it builds 
-        # from source (and takes a long time).
-        #
-        # The prebuilt package is marked as broken on aarch64-darwin
-        # haskellPackages = pkgs.haskell.packages.ghc8107;
       in
       flake // {
-        # Dev Shell
-        # devShells.default = pkgs.mkShell {
-        #   name = "fission";
-        #   buildInputs = [
-        #     stack-wrapped
-        #     haskellPackages.haskell-language-server
-        #     pkgs.cachix
-        #     pkgs.nixpkgs-fmt
-        #     pkgs.stylish-haskell
-        #     tasks
-        #   ];
-        #   NIX_PATH = "nixpkgs=" + pkgs.path;
-        # };
-
         # Built by `nix build .`
         packages.default = flake.packages."fission-cli:exe:fission";
         packages.fission-cli = flake.packages."fission-cli:exe:fission";
@@ -95,8 +74,8 @@
     # This sets the flake to use the IOG nix cache.
     # Nix should ask for permission before using it,
     # but remove it here if you do not want it to.
-    extra-substituters = [ "https://cache.iog.io" "https://cache.zw3rk.com" ];
-    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk=" ];
+    extra-substituters = [ "https://cache.iog.io" "https://fission-codes.cachix.org" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "fission-codes.cachix.org-1:z9T3OvxxngfRrx/TelcOzdnceJaCaqKQ0fby3GV1VFw=" ];
     allow-import-from-derivation = "true";
   };
 }
