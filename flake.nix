64eb03cc95faa1f6cb8ab5144b6739008b8beb67
James Walker <walkah@walkah.net>
2023-02-23 16:40:35 -0500

    rolling back to haskell.nix just for packages

--- flake.nix
+++ flake.nix
@@ -15,67 +15,83 @@
   outputs = { self, nixpkgs, flake-utils, haskellNix, flake-compat }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
       let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            # This overlay adds our project to pkgs
+            fission =
+              final.haskell-nix.project' {
+                src = pkgs.haskell-nix.haskellLib.cleanSourceWith { name = "fission"; src = ./.; };
+                compiler-nix-name = "ghc8107";
+                stack-sha256 = "0z7k4jdpwc67vpcydf3gy3rd6v1i4qp0b0im2h3da2j2zbz59gz8";
+                materialized = ./nix/materialized;
+                # checkMaterialization = true;
+
+                # NOTE: Currently handling devShells separately from haskell.nix
+                #
+                # shell.tools = {
+                #   cabal = { };
+                #   hlint = { };
+                #   haskell-language-server = { };
+                # };
+                # Non-Haskell shell tools go here
+                # shell.buildInputs = with pkgs; [
+                # ];
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.fission.flake { };
+
+        # Inspired by https://www.tweag.io/blog/2022-06-02-haskell-stack-nix-shell/
+        stack-wrapped = pkgs.symlinkJoin {
+          name = "stack";
+          paths = [ pkgs.stack ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/stack \
+              --add-flags "\
+                --nix \
+                --no-nix-pure \
+                --nix-shell-file=nix/stack-integration.nix \
+              "
+          '';
+        };
+
         # Wrapper commands for convenience
         commands = import ./nix/commands.nix;
         server-path = "~/.local/bin/fission-server";
         tasks = commands {
           inherit pkgs;
           inherit server-path;
-          # inherit stack-wrapped;
+          inherit stack-wrapped;
         };
 
-        overlays = [
-          haskellNix.overlay
-          (final: prev: {
-            # This overlay adds our project to pkgs
-            fission =
-              final.haskell-nix.project'
-                {
-                  src = pkgs.haskell-nix.haskellLib.cleanSourceWith { name = "fission"; src = ./.; };
-                  compiler-nix-name = "ghc8107";
-                  stack-sha256 = "0z7k4jdpwc67vpcydf3gy3rd6v1i4qp0b0im2h3da2j2zbz59gz8";
-                  materialized = ./nix/materialized;
-                  # checkMaterialization = true;
-
-                  # NOTE: Currently handling devShells separately from haskell.nix
-                  #
-                  shell.tools = {
-                    cabal = "3.8.1.0";
-                    # hlint = "3.4.1";
-                    stack = "2.9.1";
-                    # Currently running into https://github.com/input-output-hk/haskell.nix/issues/1830
-                    # haskell-language-server = "1.9.0.0";
-                  };
-
-                  # From: https://github.com/input-output-hk/haskell.nix/issues/1759#issuecomment-1286299368
-                  shell.additional = ps: with ps; [ Cabal ];
-
-                  # Non-Haskell shell tools go here
-                  shell.buildInputs = with pkgs; [
-                    cachix
-                    nixpkgs-fmt
-                    tasks
-                  ];
-
-                };
-          })
-        ];
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.fission.flake { };
+        # The default version of HLS (with binary cache) is built with GHC 9.0.1
+        # We can get this version working with our current set up, but it builds 
+        # from source (and takes a long time).
+        #
+        # The prebuilt package is marked as broken on aarch64-darwin
+        haskellPackages = pkgs.haskell.packages.ghc8107;
       in
       flake // {
+        # Dev Shell
+        devShells.default = pkgs.mkShell {
+          name = "fission";
+          buildInputs = [
+            stack-wrapped
+            haskellPackages.haskell-language-server
+            pkgs.cachix
+            pkgs.nixpkgs-fmt
+            pkgs.stylish-haskell
+            tasks
+          ];
+          NIX_PATH = "nixpkgs=" + pkgs.path;
+        };
+
         # Built by `nix build .`
         packages.default = flake.packages."fission-cli:exe:fission";
         packages.fission-cli = flake.packages."fission-cli:exe:fission";
         packages.fission-server = flake.packages."fission-web-server:exe:fission-server";
       });
-
-  nixConfig = {
-    # This sets the flake to use the IOG nix cache.
-    # Nix should ask for permission before using it,
-    # but remove it here if you do not want it to.
-    extra-substituters = [ "https://cache.iog.io" "https://fission-codes.cachix.org" ];
-    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "fission-codes.cachix.org-1:z9T3OvxxngfRrx/TelcOzdnceJaCaqKQ0fby3GV1VFw=" ];
-    allow-import-from-derivation = "true";
-  };
 }
